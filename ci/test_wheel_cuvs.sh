1d337e1402a5b9cd050d181a9d496e7588b97eb1
Corey J. Nolet <cjnolet@gmail.com>
2024-02-05 18:03:06 -0500

    More updates

--- ci/test_wheel_cuvs.sh
+++ ci/test_wheel_cuvs.sh
@@ -4,8 +4,8 @@
 set -euo pipefail
 
 mkdir -p ./dist
-RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen ${RAPIDS_CUDA_VERSION})"
-RAPIDS_PY_WHEEL_NAME="cuvs_${RAPIDS_PY_CUDA_SUFFIX}" rapids-download-wheels-from-s3 ./dist
+#RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen ${RAPIDS_CUDA_VERSION})"
+#RAPIDS_PY_WHEEL_NAME="cuvs_${RAPIDS_PY_CUDA_SUFFIX}" rapids-download-wheels-from-s3 ./dist
 
 ## echo to expand wildcard before adding `[extra]` requires for pip
 #python -m pip install $(echo ./dist/cuvs*.whl)[test]
