9024fb7847a0b0d9e4f3745c4f4cb292da651828
Corey J. Nolet <cjnolet@gmail.com>
2024-02-05 18:36:32 -0500

    MOre changes to get CI running. Renaming template->examples

--- ci/build_python.sh
+++ ci/build_python.sh
@@ -15,22 +15,22 @@ rapids-print-env
 
 rapids-logger "Begin py build"
 
-CPP_CHANNEL=$(rapids-download-conda-from-s3 cpp)
+#CPP_CHANNEL=$(rapids-download-conda-from-s3 cpp)
 
-version=$(rapids-generate-version)
-git_commit=$(git rev-parse HEAD)
-export RAPIDS_PACKAGE_VERSION=${version} 
-echo "${version}" > VERSION
+#version=$(rapids-generate-version)
+#git_commit=$(git rev-parse HEAD)
+#export RAPIDS_PACKAGE_VERSION=${version}
+#echo "${version}" > VERSION
 
-package_dir="python"
-for package_name in cuvs raft-dask; do
-  underscore_package_name=$(echo "${package_name}" | tr "-" "_")
-  sed -i "/^__git_commit__/ s/= .*/= \"${git_commit}\"/g" "${package_dir}/${package_name}/${underscore_package_name}/_version.py"
-done
+#package_dir="python"
+#for package_name in cuvs; do
+#  underscore_package_name=$(echo "${package_name}" | tr "-" "_")
+#  sed -i "/^__git_commit__/ s/= .*/= \"${git_commit}\"/g" "${package_dir}/${package_name}/${underscore_package_name}/_version.py"
+#done
 
 # TODO: Remove `--no-test` flags once importing on a CPU
 # node works correctly
-rapids-conda-retry mambabuild \
-  --no-test \
-  --channel "${CPP_CHANNEL}" \
-  conda/recipes/cuvs
+#rapids-conda-retry mambabuild \
+#  --no-test \
+#  --channel "${CPP_CHANNEL}" \
+#  conda/recipes/cuvs
