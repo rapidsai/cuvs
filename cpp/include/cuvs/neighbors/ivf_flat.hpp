20658f88d95d260efa3a59a371d17ffb7bb49ead
viclafargue <viclafargue@nvidia.com>
2024-02-07 14:34:12 +0100

    Adding IVF-Flat and IVF-PQ

---
+++ cpp/include/cuvs/neighbors/ivf_flat.hpp
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2024, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <raft_runtime/neighbors/ivf_flat.hpp>
+
+
+namespace cuvs::neighbors::ivf_flat {
+
+using index_params = raft::neighbors::ivf_flat::index_params;
+using search_params = raft::neighbors::ivf_flat::search_params;
+
+template <typename T, typename IdxT>
+using index = raft::neighbors::ivf_flat::index<T, IdxT>;
+
+}  // namespace cuvs::neighbors::ivf_flat
