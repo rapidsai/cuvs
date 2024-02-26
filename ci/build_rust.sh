b4dc043ea6c0bb8e1bd453c6a4cbe972ef6219b7
Ben Frederickson <ben@benfrederickson.com>
2024-02-26 11:11:17 -0800

    .

--- ci/build_rust.sh
+++ ci/build_rust.sh
@@ -3,7 +3,37 @@
 
 set -euo pipefail
 
+# install a conda environment with some build dependencies
+rapids-logger "Create test conda environment"
+. /opt/conda/etc/profile.d/conda.sh
+
+rapids-dependency-file-generator \
+  --output conda \
+  --file_key build \
+  --matrix "cuda=${RAPIDS_CUDA_VERSION%.*};arch=$(arch);py=${RAPIDS_PY_VERSION}" | tee env.yaml
+
+rapids-mamba-retry env create --force -f env.yaml -n build
+conda activate build
+
+rapids-print-env
+
+rapids-logger "Downloading artifacts from previous jobs"
+CPP_CHANNEL=$(rapids-download-conda-from-s3 cpp)
+PYTHON_CHANNEL=$(rapids-download-conda-from-s3 python)
+
+# installing libcuvs/libraft will speed up the rust build substantially
+rapids-mamba-retry install \
+  --channel "${CPP_CHANNEL}" \
+  --channel "${PYTHON_CHANNEL}" \
+  libcuvs \
+  libcuvs-headers \
+  cuvs \
+  raft-dask
+
+# install rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh  -s -- -y
 source "$HOME/.cargo/env"
+
+# build and test the rust bindings
 cd rust
 cargo test
