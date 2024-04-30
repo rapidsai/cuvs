9cbfc539e004ef051e59369f9b3ddeb1f5366370
Corey J. Nolet <cjnolet@gmail.com>
2024-04-30 18:31:04 -0400

    First commit of moving CAGRA from RAFT

--- cpp/include/cuvs/neighbors/cagra.hpp
+++ cpp/include/cuvs/neighbors/cagra.hpp
@@ -22,7 +22,6 @@
 #include <raft/core/host_device_accessor.hpp>
 #include <raft/core/mdspan.hpp>
 #include <raft/core/resources.hpp>
-#include <raft/neighbors/cagra_types.hpp>
 #include <rmm/cuda_stream_view.hpp>
 
 #include <optional>
@@ -81,7 +80,7 @@ struct vpq_params {
   double pq_kmeans_trainset_fraction = 0;
 
   /** Build a raft CAGRA index params from an existing cuvs CAGRA index params. */
-  operator raft::neighbors::vpq_params() const
+  operator cuvs::neighbors::vpq_params() const
   {
     return {.pq_bits                     = pq_bits,
             .pq_dim                      = pq_dim,
@@ -107,22 +106,6 @@ struct index_params : ann::index_params {
    * NOTE: this is experimental new API, consider it unsafe.
    */
   std::optional<vpq_params> compression = std::nullopt;
-
-  /** Build a raft CAGRA index params from an existing cuvs CAGRA index params. */
-  operator raft::neighbors::cagra::index_params() const
-  {
-    return raft::neighbors::cagra::index_params{
-      {
-        .metric            = static_cast<raft::distance::DistanceType>((int)this->metric),
-        .metric_arg        = this->metric_arg,
-        .add_data_on_build = this->add_data_on_build,
-      },
-      .intermediate_graph_degree = intermediate_graph_degree,
-      .graph_degree              = graph_degree,
-      .build_algo       = static_cast<raft::neighbors::cagra::graph_build_algo>((int)build_algo),
-      .nn_descent_niter = nn_descent_niter,
-      .compression      = compression};
-  }
 };
 
 /**
@@ -187,27 +170,6 @@ struct search_params : ann::search_params {
   uint32_t num_random_samplings = 1;
   /** Bit mask used for initial random seed node selection. */
   uint64_t rand_xor_mask = 0x128394;
-
-  /** Build a raft CAGRA search params from an existing cuvs CAGRA search params. */
-  operator raft::neighbors::cagra::search_params() const
-  {
-    raft::neighbors::cagra::search_params result = {
-      {},
-      max_queries,
-      itopk_size,
-      max_iterations,
-      static_cast<raft::neighbors::cagra::search_algo>((int)algo),
-      team_size,
-      search_width,
-      min_iterations,
-      thread_block_size,
-      static_cast<raft::neighbors::cagra::hash_mode>((int)hashmap_mode),
-      hashmap_min_bitlen,
-      hashmap_max_fill_rate,
-      num_random_samplings,
-      rand_xor_mask};
-    return result;
-  }
 };
 
 /**
@@ -233,9 +195,9 @@ static_assert(std::is_aggregate_v<search_params>);
 template <typename T, typename IdxT>
 struct index : ann::index {
   /** Build a cuvs CAGRA index from an existing RAFT CAGRA index. */
-  index(raft::neighbors::cagra::index<T, IdxT>&& raft_idx)
+  index(cuvs::neighbors::cagra::index<T, IdxT>&& raft_idx)
     : ann::index(),
-      raft_index_{std::make_unique<raft::neighbors::cagra::index<T, IdxT>>(std::move(raft_idx))}
+      raft_index_{std::make_unique<cuvs::neighbors::cagra::index<T, IdxT>>(std::move(raft_idx))}
   {
   }
   static_assert(!raft::is_narrowing_v<uint32_t, IdxT>,
@@ -287,8 +249,8 @@ struct index : ann::index {
   index(raft::resources const& res,
         cuvs::distance::DistanceType metric = cuvs::distance::DistanceType::L2Expanded)
     : ann::index(),
-      raft_index_(std::make_unique<raft::neighbors::cagra::index<T, IdxT>>(
-        res, static_cast<raft::distance::DistanceType>((int)metric)))
+      raft_index_(std::make_unique<cuvs::neighbors::cagra::index<T, IdxT>>(
+        res, static_cast<cuvs::distance::DistanceType>((int)metric)))
   {
   }
   /** Construct an index from dataset and knn_graph arrays
@@ -352,8 +314,8 @@ struct index : ann::index {
         raft::mdspan<const IdxT, raft::matrix_extent<int64_t>, raft::row_major, graph_accessor>
           knn_graph)
     : ann::index(),
-      raft_index_(std::make_unique<raft::neighbors::cagra::index<T, IdxT>>(
-        res, static_cast<raft::distance::DistanceType>((int)metric), dataset, knn_graph))
+      raft_index_(std::make_unique<cuvs::neighbors::cagra::index<T, IdxT>>(
+        res, static_cast<cuvs::distance::DistanceType>((int)metric), dataset, knn_graph))
   {
     RAFT_EXPECTS(dataset.extent(0) == knn_graph.extent(0),
                  "Dataset and knn_graph must have equal number of rows");
@@ -408,14 +370,14 @@ struct index : ann::index {
     raft_index_->update_graph(res, knn_graph);
   }
 
-  auto get_raft_index() const -> const raft::neighbors::cagra::index<T, IdxT>*
+  auto get_raft_index() const -> const cuvs::neighbors::cagra::index<T, IdxT>*
   {
     return raft_index_.get();
   }
-  auto get_raft_index() -> raft::neighbors::cagra::index<T, IdxT>* { return raft_index_.get(); }
+  auto get_raft_index() -> cuvs::neighbors::cagra::index<T, IdxT>* { return raft_index_.get(); }
 
  private:
-  std::unique_ptr<raft::neighbors::cagra::index<T, IdxT>> raft_index_;
+  std::unique_ptr<cuvs::neighbors::cagra::index<T, IdxT>> raft_index_;
 };
 
 /**
