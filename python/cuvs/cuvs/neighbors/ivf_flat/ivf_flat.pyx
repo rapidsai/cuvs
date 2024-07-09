208eac2202b0d8adf08987313c9fc968bac98eac
Mickael Ide <mide@nvidia.com>
2024-07-09 06:50:48 -0700

    Add documentation for cosine ivf flat

--- python/cuvs/cuvs/neighbors/ivf_flat/ivf_flat.pyx
+++ python/cuvs/cuvs/neighbors/ivf_flat/ivf_flat.pyx
@@ -58,12 +58,14 @@ cdef class IndexParams:
     metric : str, default = "sqeuclidean"
         String denoting the metric type.
         Valid values for metric: ["sqeuclidean", "inner_product",
-        "euclidean"], where
+        "euclidean", "cosine"], where
             - sqeuclidean is the euclidean distance without the square root
               operation, i.e.: distance(a,b) = \\sum_i (a_i - b_i)^2,
             - euclidean is the euclidean distance
             - inner product distance is defined as
               distance(a, b) = \\sum_i a_i * b_i.
+            - cosine distance is defined as
+              distance(a, b) = 1 - \\sum_i a_i * b_i / ( ||a||_2 * ||b||_2).
     kmeans_n_iters : int, default = 20
         The number of iterations searching for kmeans centers during index
         building.
