30aedf6cb63e8b98247b08a51e7e4ed0becfe59d
Corey J. Nolet <cjnolet@gmail.com>
2024-08-28 14:29:15 -0400

    Removing faiss_select

--- cpp/src/neighbors/ball_cover/registers-inl.cuh
+++ cpp/src/neighbors/ball_cover/registers-inl.cuh
@@ -21,9 +21,9 @@
 #include "registers_types.cuh"  // DistFunc
 #include <cuvs/neighbors/ball_cover.hpp>
 
-#include "../faiss_select/key_value_block_select.cuh"
 #include <raft/core/resource/cuda_stream.hpp>
 #include <raft/core/resource/thrust_policy.hpp>
+#include <raft/neighbors/detail/faiss_select/key_value_block_select.cuh>
 #include <raft/util/cuda_utils.cuh>
 
 #include <thrust/count.h>
@@ -181,7 +181,7 @@ RAFT_KERNEL compute_final_dists_registers(const value_t* X_reordered,
     local_x_ptr[j] = x_ptr[j];
   }
 
-  using namespace cuvs::neighbors::ball_cover::detail::faiss_select;
+  using namespace raft::neighbors::detail::faiss_select;
   KeyValueBlockSelect<value_t, value_idx, false, Comparator<value_t>, warp_q, thread_q, tpb> heap(
     std::numeric_limits<value_t>::max(),
     std::numeric_limits<value_t>::max(),
@@ -345,7 +345,7 @@ RAFT_KERNEL block_rbc_kernel_registers(const value_t* X_reordered,
   }
 
   // Each warp works on 1 R
-  using namespace cuvs::neighbors::ball_cover::detail::faiss_select;
+  using namespace raft::neighbors::detail::faiss_select;
   KeyValueBlockSelect<value_t, value_idx, false, Comparator<value_t>, warp_q, thread_q, tpb> heap(
     std::numeric_limits<value_t>::max(),
     std::numeric_limits<value_t>::max(),
