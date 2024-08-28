aa5f7f531be61edc74bbd7f84f6406e834bf55b8
Ajit Mistry <mail@a-mistry.com>
2024-08-28 13:35:57 +0000

    add tests

--- cpp/test/core/c_api.c
+++ cpp/test/core/c_api.c
@@ -42,7 +42,7 @@ int main()
   if (free_error == CUVS_ERROR) { exit(EXIT_FAILURE); }
 
   // Enable pool memory resource
-  cuvsError_t pool_error = cuvsRMMPoolMemoryResourceEnable(10, 100, 1);
+  cuvsError_t pool_error = cuvsRMMPoolMemoryResourceEnable(10, 100, 0);
   if (pool_error == CUVS_ERROR) { exit(EXIT_FAILURE); }
 
   // Allocate memory again
@@ -56,6 +56,23 @@ int main()
 
   // Reset pool memory resource
   cuvsError_t reset_error = cuvsRMMMemoryResourceReset();
+  if (reset_error == CUVS_ERROR) { exit(EXIT_FAILURE); }
+
+  // Enable pool memory resource (managed)
+  cuvsError_t pool_error_managed = cuvsRMMPoolMemoryResourceEnable(10, 100, 1);
+  if (pool_error == CUVS_ERROR) { exit(EXIT_FAILURE); }
+
+  // Allocate memory again
+  void* ptr3;
+  cuvsError_t alloc_error_pool_managed = cuvsRMMAlloc(res, &ptr3, 1024);
+  if (alloc_error_pool == CUVS_ERROR) { exit(EXIT_FAILURE); }
+
+  // Free memory
+  cuvsError_t free_error_pool_managed = cuvsRMMFree(res, ptr3, 1024);
+  if (free_error_pool == CUVS_ERROR) { exit(EXIT_FAILURE); }
+
+  // Reset pool memory resource
+  cuvsError_t reset_error_managed = cuvsRMMMemoryResourceReset();
   if (reset_error == CUVS_ERROR) { exit(EXIT_FAILURE); }
 
   // Destroy resources
