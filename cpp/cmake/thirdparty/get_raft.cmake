077a678febc081acd84771b7c5111b94bfb19e90
rhdong <hrong@nvidia.com>
2024-09-25 17:23:56 -0700

    Revert "To be reverted"
    This reverts commit d1c5a7178b9510bba3ab2c67cdd4fb05467f0615.

--- cpp/cmake/thirdparty/get_raft.cmake
+++ cpp/cmake/thirdparty/get_raft.cmake
@@ -61,8 +61,8 @@ endfunction()
 # To use a different RAFT locally, set the CMake variable
 # CPM_raft_SOURCE=/path/to/local/raft
 find_and_configure_raft(VERSION  ${RAFT_VERSION}.00
-        FORK                     rhdong
-        PINNED_TAG               cagra-bf
+        FORK                     ${RAFT_FORK}
+        PINNED_TAG               ${RAFT_PINNED_TAG}
         ENABLE_MNMG_DEPENDENCIES OFF
         ENABLE_NVTX              OFF
         USE_RAFT_STATIC ${CUVS_USE_RAFT_STATIC}
