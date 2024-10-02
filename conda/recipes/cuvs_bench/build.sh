f5cc01098996d93f1e1bf7575907a874c0239e9f
divyegala <divyegala@gmail.com>
2024-10-01 17:28:21 -0700

    get_cuvs.cmake to use libcuvs.so for bench

--- conda/recipes/cuvs_bench/build.sh
+++ conda/recipes/cuvs_bench/build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 # Copyright (c) 2024, NVIDIA CORPORATION.
 
+export EXTRA_CMAKE_ARGS="-DBUILD_SHARED_LIBS=OFF"
 ./build.sh bench-ann --allgpuarch --no-nvtx --build-metrics=bench_ann --incl-cache-stats
 cmake --install cpp/build --component ann_bench
