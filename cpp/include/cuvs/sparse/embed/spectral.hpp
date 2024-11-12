96ca367757ca2192fbb2b4c42fe4805a1384d640
Corey J. Nolet <cjnolet@gmail.com>
2024-11-12 13:46:23 -0500

    Adding spectral embedding

---
+++ cpp/include/cuvs/sparse/embed/spectral.hpp
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2024, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <raft/core/device_coo_matrix.hpp>
+#include <raft/core/resources.hpp>
+
+namespace cuvs::sparse::cluster::spectral {
+
+/**
+ * Given a COO formatted (symmetric) knn graph, this function computes the spectral embeddings
+ * (lowest n_components eigenvectors), using Lanczos min cut algorithm. Please note that this
+ * algorithm does not compute a full laplacian eigenmap, as the laplacian eigenmap would embed each
+ * connected component. Laplacian eigenmaps can be built from this algorithm by running it on the
+ * vectors for each connected component.
+ * @param rows source vertices of knn graph (size nnz)
+ * @param cols destination vertices of knn graph (size nnz)
+ * @param vals edge weights connecting vertices of knn graph (size nnz)
+ * @param nnz size of rows/cols/vals
+ * @param n number of samples in X
+ * @param n_neighbors the number of neighbors to query for knn graph construction
+ * @param n_components the number of components to project the X into
+ * @param out output array for embedding (size n*n_comonents)
+ */
+void embed(const raft::handle_t& handle,
+           raft::device_coo_matrix_view<float, int, int, int> knn_graph,
+           int n_components,
+           raft::device_matrix_view<float, int> out,
+           unsigned long long seed = 0L);
+{
+  cuvs::sparse::spectral::fit_embedding(handle,
+                                        knn_graph.structure_view().get_rows().data(),
+                                        knn_graph.structure_view().get_cols().data(),
+                                        knn_graph.get_elements().data(),
+                                        knn_graph.structure_view().get_nnz(),
+                                        knn_graph.structure_view().get_n_rows(),
+                                        n_components,
+                                        out.data_handle(),
+                                        seed);
+}
+};  // namespace cuvs::sparse::cluster::spectral
---
+++ cpp/src/distance/kernels.cuh
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2024, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include "detail/kernels/gram_matrix.cuh"
+#include "detail/kernels/kernel_factory.cuh"
+
+namespace cuvs::distance::kernels {
+
+// TODO: Need to expose formal APIs for this that are more consistent w/ other APIs in cuVS
+using cuvs::distance::kernels::detail::GramMatrixBase;
+using cuvs::distance::kernels::detail::KernelFactory;
+
+};  // end namespace cuvs::distance::kernels
