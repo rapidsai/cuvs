61ea9acf87117632e9dd242a13374f2a342fc643
Ishan Chattopadhyaya <ishan@apache.org>
2024-11-26 20:09:06 +0530

    Bumping version to 24.12, installing to local maven with build.sh & separate example project

--- java/build.sh
+++ java/build.sh
@@ -1,6 +1,7 @@
 export CMAKE_PREFIX_PATH=`pwd`/../cpp/build
 cd internal && cmake . && cmake --build . \
   && cd .. \
-  && mvn install:install-file -DgroupId=com.nvidia.cuvs -DartifactId=cuvs-java-internal -Dversion=0.1 -Dpackaging=so -Dfile=./internal/libcuvs_java.so \
+  && mvn install:install-file -DgroupId=com.nvidia.cuvs -DartifactId=cuvs-java-internal -Dversion=24.12 -Dpackaging=so -Dfile=./internal/libcuvs_java.so \
   && cd cuvs-java \
-  && mvn package
+  && mvn package \
+  && mvn install:install-file -Dfile=./target/cuvs-java-24.12.1-jar-with-dependencies.jar -DgroupId=com.nvidia.cuvs -DartifactId=cuvs-java -Dversion=24.12.1 -Dpackaging=jar
