2a1071b3666a99c67081b62c01626e01dda16257
Vyas Ramasubramani <vyasr@nvidia.com>
2024-11-28 15:10:58 +0000

    Preload OpenMP for TLS

--- ci/test_wheel_cuvs.sh
+++ ci/test_wheel_cuvs.sh
@@ -10,4 +10,10 @@ RAPIDS_PY_WHEEL_NAME="cuvs_${RAPIDS_PY_CUDA_SUFFIX}" rapids-download-wheels-from
 # echo to expand wildcard before adding `[extra]` requires for pip
 python -m pip install $(echo ./dist/cuvs*.whl)[test]
 
+# arm tests sporadically run into https://bugzilla.redhat.com/show_bug.cgi?id=1722181.
+# This is a workaround to ensure that OpenMP gets the TLS that it needs.
+if [[ "$(arch)" == "aarch64" ]]; then
+  export LD_PRELOAD=/opt/conda/envs/test/lib/libgomp.so.1
+fi
+
 python -m pytest ./python/cuvs/cuvs/test
