3619d3cb90ee8a041cec14e906d57e9ea3a2c33d
Vyas Ramasubramani <vyasr@nvidia.com>
2024-11-28 18:09:33 +0000

    Import sklearn since the problem is the mangled, bundled openmp

--- ci/test_wheel_cuvs.sh
+++ ci/test_wheel_cuvs.sh
@@ -10,17 +10,4 @@ RAPIDS_PY_WHEEL_NAME="cuvs_${RAPIDS_PY_CUDA_SUFFIX}" rapids-download-wheels-from
 # echo to expand wildcard before adding `[extra]` requires for pip
 python -m pip install $(echo ./dist/cuvs*.whl)[test]
 
-# arm tests sporadically run into https://bugzilla.redhat.com/show_bug.cgi?id=1722181.
-# This is a workaround to ensure that OpenMP gets the TLS that it needs.
-if [[ "$(arch)" == "aarch64" ]]; then
-  for dir in $(ld --verbose | grep SEARCH_DIR | tr -s ' ;' \\012 | grep -o '=.*"' | sed 's/[="]//g'); do
-      lib="${dir}/libgomp.so.1"
-      if [[ -f "${lib}" ]]; then
-          echo "Preloading "${lib}" to work around OpenMP TLS issue on aarch64"
-          export LD_PRELOAD="${lib}"
-          break
-      fi
-  done
-fi
-
 python -m pytest ./python/cuvs/cuvs/test
