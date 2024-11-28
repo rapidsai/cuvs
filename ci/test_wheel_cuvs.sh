66870b69728a7d36dca665dc64011d248c0db526
Vyas Ramasubramani <vyasr@nvidia.com>
2024-11-28 16:11:56 +0000

    Find the library instead of assuming a path.

--- ci/test_wheel_cuvs.sh
+++ ci/test_wheel_cuvs.sh
@@ -13,7 +13,13 @@ python -m pip install $(echo ./dist/cuvs*.whl)[test]
 # arm tests sporadically run into https://bugzilla.redhat.com/show_bug.cgi?id=1722181.
 # This is a workaround to ensure that OpenMP gets the TLS that it needs.
 if [[ "$(arch)" == "aarch64" ]]; then
-  export LD_PRELOAD=/opt/conda/envs/test/lib/libgomp.so.1
+  for dir in $(ld --verbose | grep SEARCH_DIR | tr -s ' ;' \\012 | grep -o '=.*"' | sed 's/[="]//g'); do
+      lib="${dir}/libgomp.so.1"
+      if [[ -f "${lib}" ]]; then
+          export LD_PRELOAD="${lib}"
+          break
+      fi
+  done
 fi
 
 python -m pytest ./python/cuvs/cuvs/test
