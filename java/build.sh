6f708eb1062a114c6a742426c6b7e35c58e5c80a
Ishan Chattopadhyaya <ishan@apache.org>
2024-12-14 00:21:49 +0530

    Upgrading to 25.02

--- java/build.sh
+++ java/build.sh
@@ -1,7 +1,7 @@
 export CMAKE_PREFIX_PATH=`pwd`/../cpp/build
 cd internal && cmake . && cmake --build . \
   && cd .. \
-  && mvn install:install-file -DgroupId=com.nvidia.cuvs -DartifactId=cuvs-java-internal -Dversion=24.12 -Dpackaging=so -Dfile=./internal/libcuvs_java.so \
+  && mvn install:install-file -DgroupId=com.nvidia.cuvs -DartifactId=cuvs-java-internal -Dversion=25.02 -Dpackaging=so -Dfile=./internal/libcuvs_java.so \
   && cd cuvs-java \
   && mvn package \
-  && mvn install:install-file -Dfile=./target/cuvs-java-24.12.1-jar-with-dependencies.jar -DgroupId=com.nvidia.cuvs -DartifactId=cuvs-java -Dversion=24.12.1 -Dpackaging=jar
+  && mvn install:install-file -Dfile=./target/cuvs-java-25.02.1-jar-with-dependencies.jar -DgroupId=com.nvidia.cuvs -DartifactId=cuvs-java -Dversion=54.02.1 -Dpackaging=jar
