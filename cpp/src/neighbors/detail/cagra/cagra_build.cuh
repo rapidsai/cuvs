61e20f17a22300813163d66adb7172b5ce59c81f
Hiroyuki Ootomo <hootomo@nvidia.com>
2025-01-28 03:37:43 -0800

    Add metric and build algo check

--- cpp/src/neighbors/detail/cagra/cagra_build.cuh
+++ cpp/src/neighbors/detail/cagra/cagra_build.cuh
@@ -441,6 +441,11 @@ index<T, IdxT> build(
       knn_build_params = cagra::graph_build_params::ivf_pq_params(dataset.extents(), params.metric);
     }
   }
+  RAFT_EXPECTS(
+    params.metric != BinaryHamming ||
+      std::holds_alternative<cagra::graph_build_params::iterative_search_params>(knn_build_params),
+    "IVF_PQ and NN_DESCENT for CAGRA graph build do not support BinaryHamming as a metric. Please "
+    "use the iterative CAGRA search build.");
 
   auto cagra_graph = raft::make_host_matrix<IdxT, int64_t>(0, 0);
 
