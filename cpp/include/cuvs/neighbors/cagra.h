23a1e196642fa4b8fc1977dc62068ffeb26e79a4
Ben Frederickson <github@benfrederickson.com>
2025-01-30 20:39:47 -0800

    Apply suggestions from code review

--- cpp/include/cuvs/neighbors/cagra.h
+++ cpp/include/cuvs/neighbors/cagra.h
@@ -154,9 +154,12 @@ cuvsError_t cuvsCagraCompressionParamsDestroy(cuvsCagraCompressionParams_t param
  *
  */
 struct cuvsCagraExtendParams {
-  /** Degree of input graph for pruning. */
+  /** The additional dataset is divided into chunks and added to the graph. This is the knob to
+   * adjust the tradeoff between the recall and operation throughput. Large chunk sizes can result
+   * in high throughput, but use more working memory (O(max_chunk_size*degree^2)). This can also
+   * degrade recall because no edges are added between the nodes in the same chunk. Auto select when
+   * 0. */
   uint32_t max_chunk_size;
-  /** Degree of output graph. */
 };
 
 typedef struct cuvsCagraExtendParams* cuvsCagraExtendParams_t;
