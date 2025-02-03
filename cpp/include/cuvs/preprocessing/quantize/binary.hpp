9dbb965dc943914a33b9ee1b8ee2c070ae67ab0e
enp1s0 <ootomo.h.x86@gmail.com>
2025-02-03 10:25:53 +0900

    Update binary quantization sample code

--- cpp/include/cuvs/preprocessing/quantize/binary.hpp
+++ cpp/include/cuvs/preprocessing/quantize/binary.hpp
@@ -81,9 +81,9 @@ void transform(raft::resources const& res,
  * @code{.cpp}
  * raft::handle_t handle;
  * raft::device_matrix<float, uint64_t> dataset = read_dataset(filename);
- * int64_t quantized_dim = raft::div_rounding_up_safe(dataset.extent(1), sizeof(int8_t) * 8);
+ * int64_t quantized_dim = raft::div_rounding_up_safe(dataset.extent(1), sizeof(uint8_t) * 8);
  * auto quantized_dataset = raft::make_device_matrix<uint8_t, int64_t>(
- *    handle, dataset.extent(0), quantized_dim); 
+ *    handle, dataset.extent(0), quantized_dim);
  *  cuvs::preprocessing::quantize::binary::transform(handle, dataset, quantized_dataset.view());
  * @endcode
  *
@@ -103,9 +103,11 @@ void transform(raft::resources const& res,
  * Usage example:
  * @code{.cpp}
  * raft::handle_t handle;
- * auto quantized_dataset = raft::make_host_matrix<uint8_t, int64_t>(handle, samples,
- * features); cuvs::preprocessing::quantize::binary::transform(handle, dataset,
- * quantized_dataset.view());
+ * raft::host_matrix<float, uint64_t> dataset = read_dataset(filename);
+ * int64_t quantized_dim = raft::div_rounding_up_safe(dataset.extent(1), sizeof(uint8_t) * 8);
+ * auto quantized_dataset = raft::make_host_matrix<uint8_t, int64_t>(
+ *    handle, dataset.extent(0), quantized_dim);
+ *  cuvs::preprocessing::quantize::binary::transform(handle, dataset, quantized_dataset.view());
  * @endcode
  *
  * @param[in] res raft resource
@@ -124,9 +126,11 @@ void transform(raft::resources const& res,
  * Usage example:
  * @code{.cpp}
  * raft::handle_t handle;
- * auto quantized_dataset = raft::make_device_matrix<uint8_t, int64_t>(handle, samples,
- * features); cuvs::preprocessing::quantize::binary::transform(handle, dataset,
- * quantized_dataset.view());
+ * raft::device_matrix<half, uint64_t> dataset = read_dataset(filename);
+ * int64_t quantized_dim = raft::div_rounding_up_safe(dataset.extent(1), sizeof(uint8_t) * 8);
+ * auto quantized_dataset = raft::make_device_matrix<uint8_t, int64_t>(
+ *    handle, dataset.extent(0), quantized_dim);
+ *  cuvs::preprocessing::quantize::binary::transform(handle, dataset, quantized_dataset.view());
  * @endcode
  *
  * @param[in] res raft resource
@@ -145,9 +149,11 @@ void transform(raft::resources const& res,
  * Usage example:
  * @code{.cpp}
  * raft::handle_t handle;
- * auto quantized_dataset = raft::make_host_matrix<uint8_t, int64_t>(handle, samples,
- * features); cuvs::preprocessing::quantize::binary::transform(handle, dataset,
- * quantized_dataset.view());
+ * raft::host_matrix<half, uint64_t> dataset = read_dataset(filename);
+ * int64_t quantized_dim = raft::div_rounding_up_safe(dataset.extent(1), sizeof(uint8_t) * 8);
+ * auto quantized_dataset = raft::make_host_matrix<uint8_t, int64_t>(
+ *    handle, dataset.extent(0), quantized_dim);
+ *  cuvs::preprocessing::quantize::binary::transform(handle, dataset, quantized_dataset.view());
  * @endcode
  *
  * @param[in] res raft resource
