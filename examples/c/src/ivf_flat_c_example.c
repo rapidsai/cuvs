e1a00d706b732353868ae8fddb0a1e7b48ad6b5b
Ben Frederickson <ben@benfrederickson.com>
2025-02-06 14:21:42 -0800

    update c example

--- examples/c/src/ivf_flat_c_example.c
+++ examples/c/src/ivf_flat_c_example.c
@@ -67,8 +67,12 @@ void ivf_flat_build_search_simple(cuvsResources_t *res, DLManagedTensor * datase
     search_params->n_probes = 50;
 
     // Search the `index` built using `ivfFlatBuild`
+    cuvsFilter filter;
+    filter.type = NO_FILTER;
+    filter.addr = (uintptr_t)NULL;
+
     cuvsError_t search_status = cuvsIvfFlatSearch(*res, search_params, index,
-     queries_tensor, &neighbors_tensor, &distances_tensor);
+     queries_tensor, &neighbors_tensor, &distances_tensor, filter);
     if (build_status != CUVS_SUCCESS) {
         printf("%s.\n", cuvsGetLastErrorText());
     }
