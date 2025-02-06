999f40d7740f2930a579b31bdf5779fdcb546bd3
Gil Forsyth <gforsyth@users.noreply.github.com>
2025-02-06 09:59:24 -0500

    Add `shellcheck` to pre-commit and fix warnings (#662)
    `shellcheck` is a fast, static analysis tool for shell scripts. It's good at
    flagging up unused variables, unintentional glob expansions, and other potential
    execution and security headaches that arise from the wonders of `bash` (and
    other shlangs).
    
    This PR adds a `pre-commit` hook to run `shellcheck` on all of the `sh-lang`
    files in the `ci/` directory, and the changes requested by `shellcheck` to make
    the existing files pass the check.
    
    xref: rapidsai/build-planning#135
    
    Authors:
      - Gil Forsyth (https://github.com/gforsyth)
    
    Approvers:
      - James Lamb (https://github.com/jameslamb)
    
    URL: https://github.com/rapidsai/cuvs/pull/662

--- ci/build_wheel_cuvs.sh
+++ ci/build_wheel_cuvs.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 
 package_dir="python/cuvs"
 
-RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen ${RAPIDS_CUDA_VERSION})"
+RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen "${RAPIDS_CUDA_VERSION}")"
 
 # Downloads libcuvs wheels from this current build,
 # then ensures 'cuvs' wheel builds always use the 'libcuvs' just built in the same CI run.
--- ci/test_wheel_cuvs.sh
+++ ci/test_wheel_cuvs.sh
@@ -4,7 +4,7 @@
 set -euo pipefail
 
 mkdir -p ./dist
-RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen ${RAPIDS_CUDA_VERSION})"
+RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen "${RAPIDS_CUDA_VERSION}")"
 RAPIDS_PY_WHEEL_NAME="libcuvs_${RAPIDS_PY_CUDA_SUFFIX}" rapids-download-wheels-from-s3 cpp ./local-libcuvs-dep
 RAPIDS_PY_WHEEL_NAME="cuvs_${RAPIDS_PY_CUDA_SUFFIX}" rapids-download-wheels-from-s3 python ./dist
 
