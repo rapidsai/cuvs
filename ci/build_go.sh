d948aec8beca9b842345b50356b33b3649f6af2f
Kyle Edwards <kyedwards@nvidia.com>
2025-02-14 14:23:03 -0500

    Consolidate more Conda solves in CI
    Issue: https://github.com/rapidsai/build-planning/issues/22

--- ci/build_go.sh
+++ ci/build_go.sh
@@ -3,15 +3,18 @@
 
 set -euo pipefail
 
+rapids-logger "Downloading artifacts from previous jobs"
+CPP_CHANNEL=$(rapids-download-conda-from-s3 cpp)
+
 rapids-logger "Create test conda environment"
 . /opt/conda/etc/profile.d/conda.sh
-
-RAPIDS_VERSION="$(rapids-version)"
 
 rapids-dependency-file-generator \
   --output conda \
   --file-key go \
-  --matrix "cuda=${RAPIDS_CUDA_VERSION%.*};arch=$(arch);py=${RAPIDS_PY_VERSION}" | tee env.yaml
+  --matrix "cuda=${RAPIDS_CUDA_VERSION%.*};arch=$(arch);py=${RAPIDS_PY_VERSION}" \
+  --prepend-channel "${CPP_CHANNEL}" \
+  | tee env.yaml
 
 rapids-mamba-retry env create --yes -f env.yaml -n go
 
@@ -27,13 +30,5 @@ export CGO_CFLAGS="-I${CONDA_PREFIX}/include"
 export CGO_LDFLAGS="-L${CONDA_PREFIX}/lib -lcudart -lcuvs -lcuvs_c"
 export LD_LIBRARY_PATH="$CONDA_PREFIX/lib:$LD_LIBRARY_PATH"
 export CC=clang
-
-rapids-logger "Downloading artifacts from previous jobs"
-CPP_CHANNEL=$(rapids-download-conda-from-s3 cpp)
-
-rapids-mamba-retry install \
-  --channel "${CPP_CHANNEL}" \
-  "libcuvs=${RAPIDS_VERSION}" \
-  "libraft=${RAPIDS_VERSION}"
 
 bash ./build.sh go
