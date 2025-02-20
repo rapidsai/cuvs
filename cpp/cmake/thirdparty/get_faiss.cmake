3bc38c65b7e068469b7477b95c9098783584ad41
Tarang Jain <jaintarang2015@gmail.com>
2025-02-20 05:14:44 -0800

    blas vendor + downgrade mkl

--- cpp/cmake/thirdparty/get_faiss.cmake
+++ cpp/cmake/thirdparty/get_faiss.cmake
@@ -54,7 +54,7 @@ function(find_and_configure_faiss)
     GIT_SHALLOW ${shallow} ${patch_command}
     EXCLUDE_FROM_ALL ${exclude}
     OPTIONS
-    "FAISS_ENABLE_MKL OFF"
+    "BLA_VENDOR Intel10_64ilp"
     "FAISS_ENABLE_GPU ${PKG_ENABLE_GPU}"
     "FAISS_ENABLE_CUVS ${PKG_ENABLE_GPU}"
     "FAISS_ENABLE_PYTHON OFF"
