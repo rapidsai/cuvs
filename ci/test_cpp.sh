c28e95fbf40ad7e189f8c5088de14dc0908f9ae6
rhdong <rhdong2017@gmail.com>
2025-02-28 17:00:23 -0800

    Revert "Create Conda CI test env in one step (#684)"
    This reverts commit 211deb58a380aa385b439e1dae2c8ad15d018452.

--- ci/test_cpp.sh
+++ ci/test_cpp.sh
@@ -5,15 +5,13 @@ set -euo pipefail
 
 . /opt/conda/etc/profile.d/conda.sh
 
-CPP_CHANNEL=$(rapids-download-conda-from-s3 cpp)
+RAPIDS_VERSION="$(rapids-version)"
 
 rapids-logger "Generate C++ testing dependencies"
 rapids-dependency-file-generator \
   --output conda \
   --file-key test_cpp \
-  --matrix "cuda=${RAPIDS_CUDA_VERSION%.*};arch=$(arch)" \
-  --prepend-channel "${CPP_CHANNEL}" \
-  | tee env.yaml
+  --matrix "cuda=${RAPIDS_CUDA_VERSION%.*};arch=$(arch)" | tee env.yaml
 
 rapids-mamba-retry env create --yes -f env.yaml -n test
 
@@ -22,10 +20,16 @@ set +u
 conda activate test
 set -u
 
+CPP_CHANNEL=$(rapids-download-conda-from-s3 cpp)
 RAPIDS_TESTS_DIR=${RAPIDS_TESTS_DIR:-"${PWD}/test-results"}/
 mkdir -p "${RAPIDS_TESTS_DIR}"
 
 rapids-print-env
+
+rapids-mamba-retry install \
+  --channel "${CPP_CHANNEL}" \
+  "libcuvs=${RAPIDS_VERSION}" \
+  "libcuvs-tests=${RAPIDS_VERSION}"
 
 rapids-logger "Check GPU usage"
 nvidia-smi
