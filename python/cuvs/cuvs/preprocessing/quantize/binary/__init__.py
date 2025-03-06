64228163bfd8185dbace70e070b6f3cf5683fce5
Hiroyuki Ootomo <hootomo@nvidia.com>
2025-03-06 18:31:25 +0900

    Fix python modules

--- python/cuvs/cuvs/preprocessing/quantize/binary/__init__.py
+++ python/cuvs/cuvs/preprocessing/quantize/binary/__init__.py
@@ -12,6 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from .binary import transform
+from .binary import QuantizerParams, transform
 
-__all__ = ["transform"]
+__all__ = [
+    "QuantizerParams",
+    "transform",
+]
