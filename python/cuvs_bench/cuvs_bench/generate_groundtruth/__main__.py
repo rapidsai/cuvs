2fef158679414ddcd968853815266f9cb57ae609
Rohan Varma <rohanv@nvidia.com>
2025-03-26 15:08:26 -0700

    fix code style

--- python/cuvs_bench/cuvs_bench/generate_groundtruth/__main__.py
+++ python/cuvs_bench/cuvs_bench/generate_groundtruth/__main__.py
@@ -84,9 +84,9 @@ def force_fallback_to_numpy():
 if rmm is not None:
     gpu_system = True
     try:
-        from cuvs.common import Resources
         from rmm.allocators.cupy import rmm_cupy_allocator
 
+        from cuvs.common import Resources
         from cuvs.neighbors.brute_force import build, search
     except ImportError:
         # RMM is available, cupy is available, but cuVS is not
