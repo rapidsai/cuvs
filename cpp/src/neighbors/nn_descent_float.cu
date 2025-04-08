c57210495259075e3fa988291bb1a7af6bafcfc7
jinsolp <jinsolp@nvidia.com>
2025-04-08 21:04:54 +0000

    newline

--- cpp/src/neighbors/nn_descent_float.cu
+++ cpp/src/neighbors/nn_descent_float.cu
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 #include "nn_descent.cuh"
 #include <cuvs/neighbors/nn_descent.hpp>
 
