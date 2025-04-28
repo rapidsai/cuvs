5bf89615e07c582716977b1dc5b15b7837d25de8
Vivek Narang <vivek@searchscale.com>
2025-04-28 18:00:36 +0530

    build c wrapper before generating panama bindings

--- java/panama-bindings/generate-bindings.sh
+++ java/panama-bindings/generate-bindings.sh
@@ -7,29 +7,29 @@ CUDA_HOME=$(which nvcc | cut -d/ -f-4)
 TARGET_PACKAGE="com.nvidia.cuvs.internal.panama"
 
 # Try to verify that a include directory exists inside CUDA_HOME
-if [ ! -d "$CUDA_HOME/include" ]; 
+if [ ! -d "$CUDA_HOME/include" ];
 then
-	echo "$CUDA_HOME/include does not exist."
-	if [ -d "/usr/local/cuda/include" ];
-	then
-		echo "Setting CUDA_HOME to /usr/local/cuda"
-		CUDA_HOME=/usr/local/cuda
-	else
-		echo "Couldn't find a suitable CUDA include directory."
-		exit 1
-	fi
+  echo "$CUDA_HOME/include does not exist."
+  if [ -d "/usr/local/cuda/include" ];
+  then
+    echo "Setting CUDA_HOME to /usr/local/cuda"
+    CUDA_HOME=/usr/local/cuda
+  else
+    echo "Couldn't find a suitable CUDA include directory."
+    exit 1
+  fi
 fi
 
 JEXTRACT_COMMAND="jextract"
 
 if [[ `command -v jextract` == "" ]];
 then
-   JEXTRACT_DOWNLOAD_URL="https://download.java.net/java/early_access/jextract/22/6/openjdk-22-jextract+6-47_linux-x64_bin.tar.gz"
-   echo "jextract doesn't exist. Downloading it from $JEXTRACT_DOWNLOAD_URL.";
-   wget -c $JEXTRACT_DOWNLOAD_URL
-   tar -xvf openjdk-22-jextract+6-47_linux-x64_bin.tar.gz
-   JEXTRACT_COMMAND="jextract-22/bin/jextract"
-   echo "jextract downloaded to `pwd`/jextract-22"
+  JEXTRACT_DOWNLOAD_URL="https://download.java.net/java/early_access/jextract/22/6/openjdk-22-jextract+6-47_linux-x64_bin.tar.gz"
+  echo "jextract doesn't exist. Downloading it from $JEXTRACT_DOWNLOAD_URL.";
+  wget -c $JEXTRACT_DOWNLOAD_URL
+  tar -xvf openjdk-22-jextract+6-47_linux-x64_bin.tar.gz
+  JEXTRACT_COMMAND="jextract-22/bin/jextract"
+  echo "jextract downloaded to `pwd`/jextract-22"
 fi
 
 # Debug printing
@@ -42,7 +42,7 @@ echo "REPODIR is: $REPODIR"
 
 # Use Jextract utility to generate panama bindings
 $JEXTRACT_COMMAND \
- --include-dir ${REPODIR}/cpp/build/_deps/dlpack-src/include/ \
+ --include-dir ${REPODIR}/java/internal/_deps/dlpack-src/include/ \
  --include-dir ${CUDA_HOME}/include \
  --include-dir ${REPODIR}/cpp/include \
  --output "${REPODIR}/java/cuvs-java/src/main/java22/" \
