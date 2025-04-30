24f127ae7d065e31423b97bf2d8840c4ffcdf230
Vivek Narang <vivek@searchscale.com>
2025-05-01 01:30:06 +0530

    review updates

--- java/panama-bindings/generate-bindings.sh
+++ java/panama-bindings/generate-bindings.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e -u -o pipefail
+
 echo "Starting Panama FFM API bindings generation ..."
 REPODIR=$(cd $(dirname $0); cd ../../ ; pwd)
 CURDIR=$(cd $(dirname $0); pwd)
@@ -20,44 +22,25 @@ then
   fi
 fi
 
-JEXTRACT_COMMAND="jextract"
-
 if [[ `command -v jextract` == "" ]];
 then
-  JEXTRACT_DOWNLOAD_URL="https://download.java.net/java/early_access/jextract/22/6/openjdk-22-jextract+6-47_linux-x64_bin.tar.gz"
+  JEXTRACT_FILENAME="openjdk-22-jextract+6-47_linux-x64_bin.tar.gz"
+  JEXTRACT_DOWNLOAD_URL="https://download.java.net/java/early_access/jextract/22/6/${JEXTRACT_FILENAME}"
   echo "jextract doesn't exist. Downloading it from $JEXTRACT_DOWNLOAD_URL.";
   wget -c $JEXTRACT_DOWNLOAD_URL
-  tar -xvf openjdk-22-jextract+6-47_linux-x64_bin.tar.gz
-  JEXTRACT_COMMAND="jextract-22/bin/jextract"
-  echo "jextract downloaded to `pwd`/jextract-22"
+  tar -xvf ./"${JEXTRACT_FILENAME}"
+  export PATH="$(pwd)/jextract-22/bin/jextract:${PATH}"
+  echo "jextract downloaded to $(pwd)/jextract-22"
 fi
 
-# Debug printing
-echo "CUDA_HOME points to: $CUDA_HOME"
-echo "include dir in CUDA_HOME has:"
-ls $CUDA_HOME/include
-echo "JEXTRACT_COMMAND points to: $JEXTRACT_COMMAND"
-echo "CURDIR is: $CURDIR"
-echo "REPODIR is: $REPODIR"
-
 # Use Jextract utility to generate panama bindings
-$JEXTRACT_COMMAND \
- --include-dir ${REPODIR}/java/internal/_deps/dlpack-src/include/ \
- --include-dir ${CUDA_HOME}/include \
+jextract \
+ --include-dir ${REPODIR}/java/internal/build/_deps/dlpack-src/include/ \
+ --include-dir ${CUDA_HOME}/targets/x86_64-linux/include \
  --include-dir ${REPODIR}/cpp/include \
  --output "${REPODIR}/java/cuvs-java/src/main/java22/" \
  --target-package ${TARGET_PACKAGE} \
  --header-class-name PanamaFFMAPI \
  ${CURDIR}/headers.h
-
-# Did Jextract complete normally? If not, stop and return
-JEXTRACT_RETURN_VALUE=$?
-if [ $JEXTRACT_RETURN_VALUE == 0 ]
-then
-  echo "Jextract SUCCESS"
-else
-  echo "Jextract encountered issues (returned value ${JEXTRACT_RETURN_VALUE})"
-  exit $JEXTRACT_RETURN_VALUE
-fi
 
 echo "Panama FFM API bindings generation done"
