f31c2894701d3f66710f0006a3a49e64a92fe2db
rhdong <rhdong2017@gmail.com>
2025-05-02 11:19:42 -0700

    update version in java .md files

--- ci/release/update-version.sh
+++ ci/release/update-version.sh
@@ -106,3 +106,6 @@ sed_runner "s/VERSION=\".*\"/VERSION=\"${NEXT_FULL_JAVA_TAG}\"/g" java/build.sh
 for FILE in java/*/pom.xml; do
   sed_runner "/<!--CUVS_JAVA#VERSION_UPDATE_MARKER_START-->.*<!--CUVS_JAVA#VERSION_UPDATE_MARKER_END-->/s//<!--CUVS_JAVA#VERSION_UPDATE_MARKER_START--><version>${NEXT_FULL_JAVA_TAG}<\/version><!--CUVS_JAVA#VERSION_UPDATE_MARKER_END-->/g" "${FILE}"
 done
+
+sed_runner "s| [0-9][0-9].[0-9][0-9]| ${NEXT_SHORT_TAG}|g" java/README.md
+sed_runner "s|-[0-9][0-9].[0-9][0-9].[0-9]|-${NEXT_FULL_JAVA_TAG}|g" java/examples/README.md
