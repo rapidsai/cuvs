ed474d1d6491ff3e91a12c8a37e80a2464cf90ff
Ben Frederickson <ben@benfrederickson.com>
2025-05-13 15:33:09 -0700

    move extract_centers to header

--- cpp/src/neighbors/ivf_pq/ivf_pq_build_common.cu
+++ cpp/src/neighbors/ivf_pq/ivf_pq_build_common.cu
@@ -290,30 +290,6 @@ void set_centers(raft::resources const& handle,
   detail::set_centers(handle, index, cluster_centers.data_handle());
 }
 
-namespace detail {
-template <typename output_mdspan_type>
-inline void extract_centers(raft::resources const& res,
-                            const cuvs::neighbors::ivf_pq::index<int64_t>& index,
-                            output_mdspan_type cluster_centers)
-{
-  RAFT_EXPECTS(cluster_centers.extent(0) == index.n_lists(),
-               "Number of rows in the output buffer for cluster centers must be equal to the "
-               "number of IVF lists");
-  RAFT_EXPECTS(
-    cluster_centers.extent(1) == index.dim(),
-    "Number of columns in the output buffer for cluster centers and index dim are different");
-  auto stream = raft::resource::get_cuda_stream(res);
-  RAFT_CUDA_TRY(cudaMemcpy2DAsync(cluster_centers.data_handle(),
-                                  sizeof(float) * index.dim(),
-                                  index.centers().data_handle(),
-                                  sizeof(float) * index.dim_ext(),
-                                  sizeof(float) * index.dim(),
-                                  index.n_lists(),
-                                  cudaMemcpyDefault,
-                                  stream));
-}
-}  // namespace detail
-
 void extract_centers(raft::resources const& res,
                      const cuvs::neighbors::ivf_pq::index<int64_t>& index,
                      raft::device_matrix_view<float, uint32_t, raft::row_major> cluster_centers)
