33e70ac326325f4728b40ef74fc4b16d3483ac05
Vinay D <vinayd@nvidia.com>
2025-05-28 17:03:16 +0100

    Removing PCG as it is no longer needed in this code

--- cpp/bench/prims/src/distance/common.cuh
+++ cpp/bench/prims/src/distance/common.cuh
@@ -129,62 +129,6 @@ public:
     }
 };
 
-// Host RNG for filling a buffer
-class PCG {
-public:
-  // The constructor
-  PCG(uint64_t seed, uint64_t subsequence)
-  {
-    pcg_state = uint64_t(0);
-    inc       = (subsequence << 1u) | 1u;
-    clock();
-    pcg_state += seed;
-    clock();
-  }
-
-  // Get a single uint32_t value based on the state of the PCG
-  void next(uint32_t& ret) {
-    uint32_t xorshifted = ((pcg_state >> 18u) ^ pcg_state) >> 27u;
-    uint32_t rot        = pcg_state >> 59u;
-    ret                 = (xorshifted >> rot) | (xorshifted << ((-rot) & 31));
-    clock();
-    return;
-  }
-
-  void next(float& ret) {
-    uint32_t val = 0;
-    next(val);
-    val >>= 8;
-    ret   = static_cast<float>(val) / float(uint32_t(1) << 24);
-  }
-
-  void next(double& ret) {
-    uint32_t val_lo = 0;
-    uint32_t val_hi = 0;
-    next(val_lo);
-    next(val_hi);
-    uint64_t val = uint64_t(val_hi) << 32 | uint64_t(val_lo);
-    val >>= 11;
-    ret   = static_cast<double>(val) / double(uint64_t(1) << 53);
-  }
-
-  template <typename T>
-  void fill_buffer(T* buff, size_t n_items) {
-    for (size_t i = 0; i < n_items; i++) {
-      T rng_val;
-      next(rng_val);
-      buff[i] = rng_val;
-    }
-  }
-  // Move ahead PCG state by a single step
-  void clock() {
-    pcg_state = pcg_state * 6364136223846793005ULL + inc;
-  }
-
-private:
-  uint64_t pcg_state;
-  uint64_t inc;
-};
 
 template <typename DataT, typename AccT, typename OutT, typename IdxT>
 void ref_l2nn(OutT* out, const DataT* A, const DataT* B, IdxT M, IdxT N, IdxT K) {
