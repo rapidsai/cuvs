7901903449c28e09aa144c9a981ea3055f514e40
James Xia <jamxia@nvidia.com>
2025-05-30 16:05:38 -0700

    Fetch codebooks from url

--- cpp/tests/neighbors/ann_vamana.cuh
+++ cpp/tests/neighbors/ann_vamana.cuh
@@ -138,10 +138,13 @@ class AnnVamanaTest : public ::testing::TestWithParam<AnnVamanaInputs> {
     index_params.visited_size      = ps.visited_size;
     index_params.max_fraction      = ps.max_fraction;
     index_params.reverse_batchsize = ps.reverse_batchsize;
-    // use codebooks with dim=384 to test serialization & quantization code path
-    if (ps.dim == 384)
-      index_params.codebooks = vamana::deserialize_codebooks(
-        std::string(TEST_DATA_DIR) + "neighbors/ann_vamana/quantizer_fbv6_384_64", ps.dim);
+    // use randomized codebooks to test serialization & quantization code path
+    if (ps.dim == 384 && std::is_same_v<DataT, int8_t>)
+      index_params.codebooks =
+        vamana::deserialize_codebooks(std::string(CODEBOOKS_DIR) + "384_int8", ps.dim);
+    if (ps.dim == 64 && std::is_same_v<DataT, float>)
+      index_params.codebooks =
+        vamana::deserialize_codebooks(std::string(CODEBOOKS_DIR) + "64_float", ps.dim);
 
     auto database_view = raft::make_device_matrix_view<const DataT, int64_t>(
       (const DataT*)database.data(), ps.n_rows, ps.dim);
@@ -297,7 +300,7 @@ inline std::vector<AnnVamanaInputs> generate_inputs()
 
   std::vector<AnnVamanaInputs> inputs2 = raft::util::itertools::product<AnnVamanaInputs>(
     {1000},
-    {1, 3, 5, 7, 8, 17, 64, 128, 137, 192, 256, 512, 619, 1024},
+    {1, 3, 5, 7, 8, 17, 64, 128, 137, 192, 256, 384, 512, 619, 1024},
     {64},        // graph degree
     {128, 512},  // visited_size
     {0.06},
@@ -315,7 +318,7 @@ inline std::vector<AnnVamanaInputs> generate_inputs()
 
   inputs2 = raft::util::itertools::product<AnnVamanaInputs>(
     {1000},
-    {1, 3, 5, 7, 8, 17, 64, 128, 137, 192, 256, 512, 619, 1024},
+    {1, 3, 5, 7, 8, 17, 64, 128, 137, 192, 256, 384, 512, 619, 1024},
     {128},  // graph degree
     {256},  // visited_size
     {0.06},
@@ -333,7 +336,7 @@ inline std::vector<AnnVamanaInputs> generate_inputs()
 
   inputs2 = raft::util::itertools::product<AnnVamanaInputs>(
     {1000},
-    {1, 3, 5, 7, 8, 17, 64, 128, 137, 192, 256, 512, 619, 1024},
+    {1, 3, 5, 7, 8, 17, 64, 128, 137, 192, 256, 384, 512, 619, 1024},
     {256},        // graph degree
     {512, 1024},  // visited_size
     {0.06},
