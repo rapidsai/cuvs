6a05961581b1c46339daba5c85f64286da032845
viclafargue <viclafargue@nvidia.com>
2025-06-16 13:34:38 +0200

    Update SNMG ANN API testing

--- cpp/tests/neighbors/mg.cuh
+++ cpp/tests/neighbors/mg.cuh
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023, NVIDIA CORPORATION.
+ * Copyright (c) 2023-2025, NVIDIA CORPORATION.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -686,8 +686,6 @@ const std::vector<AnnMGInputs> inputs = {
    1024,
    cuvs::distance::DistanceType::L2Expanded,
    true},
-
-  /*
   {7000,
    10000,
    8,
@@ -699,9 +697,6 @@ const std::vector<AnnMGInputs> inputs = {
    1024,
    cuvs::distance::DistanceType::L2Expanded,
    true},
-  */
-
-  /*
   {7000,
    10000,
    8,
@@ -735,6 +730,10 @@ const std::vector<AnnMGInputs> inputs = {
    1024,
    cuvs::distance::DistanceType::L2Expanded,
    true},
+  /*
+  Tree merge requires an even number of GPU, but CI only has one. Disabled for now.
+  See https://github.com/rapidsai/cuvs/issues/904
+
   {7000,
    10000,
    8,
@@ -769,7 +768,6 @@ const std::vector<AnnMGInputs> inputs = {
    cuvs::distance::DistanceType::L2Expanded,
    true},
    */
-
   {7000,
    10000,
    8,
@@ -792,8 +790,6 @@ const std::vector<AnnMGInputs> inputs = {
    1024,
    cuvs::distance::DistanceType::L2Expanded,
    true},
-
-  /*
   {7000,
    10000,
    8,
@@ -805,8 +801,6 @@ const std::vector<AnnMGInputs> inputs = {
    1024,
    cuvs::distance::DistanceType::L2Expanded,
    true},
-  */
-
   {3,
    10000,
    8,
@@ -829,8 +823,6 @@ const std::vector<AnnMGInputs> inputs = {
    1024,
    cuvs::distance::DistanceType::L2Expanded,
    true},
-
-  /*
   {3,
    10000,
    8,
@@ -842,6 +834,5 @@ const std::vector<AnnMGInputs> inputs = {
    1024,
    cuvs::distance::DistanceType::L2Expanded,
    true},
-  */
 };
 }  // namespace cuvs::neighbors::mg
