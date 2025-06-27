a4a8211424bce0f0d2e6fae108c344782b138fe8
Kyle Edwards <kyedwards@nvidia.com>
2025-06-27 16:57:12 -0400

    Dynamically read from VERSION file for Java builds

---
+++ java/get-version.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Copyright (c) 2025, NVIDIA CORPORATION.
+
+set -euo pipefail
+
+RAPIDS_VERSION="$(rapids-version)"
+export RAPIDS_VERSION
+RAPIDS_VERSION_MAJOR_MINOR="$(rapids-version-major-minor)"
+
+echo "$RAPIDS_VERSION_MAJOR_MINOR.$(python3 -c "from packaging.version import Version; import os; print(Version(os.getenv('RAPIDS_VERSION')).micro)")"
