c194f72069c59e4405ecd52c19d0a2ad3a2752ce
Gil Forsyth <gforsyth@users.noreply.github.com>
2025-07-17 15:25:04 -0700

    fix(docker): use versioned `-latest` tag for all `rapidsai` images (#1129)
    In rapidsai/build-planning#187 we switched the docker image tagging scheme
    over to include the CalVer information.  This was done to allow us to make
    changes to the images during burndown without breaking release pipelines.
    
    This PR moves all of the existing `latest` tags to the newer versioned tag
    `25.08-latest` and also modifies the `update_version.sh` script to bump
    that version at branch creation time.
    
    xref: https://github.com/rapidsai/build-planning/issues/187
    
    Authors:
      - Gil Forsyth (https://github.com/gforsyth)
      - James Lamb (https://github.com/jameslamb)
    
    Approvers:
      - James Lamb (https://github.com/jameslamb)
    
    URL: https://github.com/rapidsai/cuvs/pull/1129

--- ci/release/update-version.sh
+++ ci/release/update-version.sh
@@ -73,6 +73,7 @@ done
 
 for FILE in .github/workflows/*.yaml; do
   sed_runner "/shared-workflows/ s/@.*/@branch-${NEXT_SHORT_TAG}/g" "${FILE}"
+  sed_runner "s/:[0-9]*\\.[0-9]*-/:${NEXT_SHORT_TAG}-/g" "${FILE}"
 done
 
 sed_runner "/rapidsai\/raft/ s|branch-[0-9][0-9].[0-9][0-9]|branch-${NEXT_SHORT_TAG}|g" docs/source/developer_guide.md
