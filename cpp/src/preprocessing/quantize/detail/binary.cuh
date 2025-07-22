1a1cda1d3a9aac6be2e3e21735a5da2ff94ff735
Corey J. Nolet <cjnolet@gmail.com>
2025-07-22 16:32:27 -0400

    Initial commit for removing cuda 11 from codebase

--- cpp/src/preprocessing/quantize/detail/binary.cuh
+++ cpp/src/preprocessing/quantize/detail/binary.cuh
@@ -419,10 +419,8 @@ void transform(raft::resources const& res,
                dataset_size,
                out_dataset_size);
 
-  // Use `float` for computation when T == half because a runtime error occurs with CUDA 11.8
-  using compute_t          = std::conditional_t<std::is_same_v<half, T>, float, T>;
-  auto threshold_vec       = raft::make_host_vector<compute_t, int64_t>(0);
-  compute_t* threshold_ptr = nullptr;
+  auto threshold_vec       = raft::make_host_vector<T, int64_t>(0);
+  T* threshold_ptr = nullptr;
 
   if (quantizer.threshold.size() != 0) {
     threshold_vec = raft::make_host_vector<compute_t, int64_t>(dataset_dim);
