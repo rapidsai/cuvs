82409f01d6e240def9d4d3f45aa6d615df34cbf4
aamijar <aamijar230@gmail.com>
2025-07-22 18:51:10 +0000

    remove functions from public api

--- cpp/include/cuvs/preprocessing/spectral_embedding.hpp
+++ cpp/include/cuvs/preprocessing/spectral_embedding.hpp
@@ -43,31 +43,6 @@ struct params {
   uint64_t seed;
 };
 
-void create_connectivity_graph(raft::resources const& handle,
-                               params spectral_embedding_config,
-                               raft::device_matrix_view<float, int, raft::row_major> dataset,
-                               raft::device_matrix_view<float, int, raft::col_major> embedding,
-                               raft::device_coo_matrix<float, int, int, int>& connectivity_graph);
-
-raft::device_csr_matrix_view<float, int, int, int> coo_to_csr_matrix(
-  raft::resources const& handle,
-  const int n_samples,
-  raft::device_vector_view<int> sym_coo_row_ind,
-  raft::device_coo_matrix<float, int, int, int>& sym_coo_matrix);
-
-raft::device_csr_matrix<float, int, int, int> create_laplacian(
-  raft::resources const& handle,
-  params spectral_embedding_config,
-  raft::device_csr_matrix_view<float, int, int, int> csr_matrix_view,
-  raft::device_vector_view<float, int> diagonal);
-
-void compute_eigenpairs(raft::resources const& handle,
-                        params spectral_embedding_config,
-                        const int n_samples,
-                        raft::device_csr_matrix<float, int, int, int> laplacian,
-                        raft::device_vector_view<float, int> diagonal,
-                        raft::device_matrix_view<float, int, raft::col_major> embedding);
-
 void transform(raft::resources const& handle,
                params config,
                raft::device_matrix_view<float, int, raft::row_major> dataset,
