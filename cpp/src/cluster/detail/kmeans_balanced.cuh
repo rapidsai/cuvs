e3e8c842d15c12f11434eb7ebcac08719d724162
Vinay D <vinayd@nvidia.com>
2025-07-24 18:34:57 +0100

    Removing inline checks

--- cpp/src/cluster/detail/kmeans_balanced.cuh
+++ cpp/src/cluster/detail/kmeans_balanced.cuh
@@ -64,105 +64,6 @@
 namespace cuvs::cluster::kmeans::detail {
 
 constexpr static inline float kAdjustCentersWeight = 7.0f;
-class ComparisonSummary {
-public:
-  double max_diff;          // Maximum difference found
-  uint64_t max_diff_index;   // where does the maximim difference occur
-  double max_a;
-  double max_b;
-  double acc_diff;          // sum of all the diffs
-  uint64_t n;               // How many items are compared
-  uint64_t n_misses;
-  int mutex;                // Simple mutex lock for thread synchronization
-
-  __device__ __host__
-  void init() {
-    max_diff = 0.0;
-    max_diff_index = 0;
-    max_a = 0.0;
-    max_b = 0.0;
-    acc_diff = 0.0;
-    n = 0;
-    n_misses = 0;
-  }
-
-  __device__ __host__
-  void update(double diff, uint64_t index, double a_val, double b_val, bool missed) {
-    if ( max_diff < diff ) {
-      max_diff = diff;
-      max_diff_index = index;
-      max_a = a_val;
-      max_b = b_val;
-    }
-    acc_diff += diff;
-    n++;
-    n_misses = missed ? n_misses + 1 : n_misses;
-  }
-
-  __device__ __host__
-  void update(ComparisonSummary& op2) {
-    if ( max_diff < op2.max_diff ) {
-      max_diff = op2.max_diff;
-      max_diff_index = op2.max_diff_index;
-      max_a = op2.max_a;
-      max_b = op2.max_b;
-    }
-    acc_diff += op2.acc_diff;
-    n += op2.n;
-    n_misses += op2.n_misses;
-  }
-
-  __device__ __host__
-  void print() {
-    //if (max_diff > 0.0) {
-      printf("Total compared %lu\n", n);
-      printf("Total missed %lu\n", n_misses);
-      printf("Average diff: %e\n", acc_diff / n);
-      printf("max_diff: %e (%e - %e)\n", max_diff, max_a, max_b);
-      printf("max_diff_index: %lu\n", max_diff_index);
-    //}
-  }
-};
-
-template <typename OutT, typename IdxT>
-__global__ void vector_compare_kernel(const OutT* a, const OutT* b, IdxT n,
-                                      ComparisonSummary* global_summary) {
-  ComparisonSummary block_summary;
-
-  block_summary.init();
-
-  for (IdxT i = 0; i < n; i++) {
-    double diff, a_val, b_val;
-    bool missed = false;
-    if constexpr (std::is_fundamental_v<OutT> || std::is_same_v<OutT, half>) {
-      diff = std::abs(double(a[i]) - double(b[i]));
-      a_val = double(a[i]);
-      b_val = double(b[i]);
-    } else {
-      diff = std::abs(double(a[i].value) - double(b[i].value));
-      a_val = double(a[i].value);
-      b_val = double(b[i].value);
-
-      missed = a[i].key != b[i].key;
-    }
-
-    block_summary.update(diff, i, a_val, b_val, missed);
-  }
-  global_summary->update(block_summary);
-}
-
-template <typename OutT, typename IdxT>
-void vector_compare(ComparisonSummary* global_summary, const OutT* a, const OutT* b, const IdxT n, cudaStream_t stream = nullptr) {
-  constexpr int block_size = 256;
-  const int grid_size = (n + block_size - 1) / block_size;
-
-  printf("comparing %lu\n", size_t(n));
-
-  vector_compare_kernel<OutT, IdxT><<<1, 1, 0, stream>>>(a, b, n, global_summary);
-
-  cudaStreamSynchronize(stream);
-  global_summary->print();
-}
 
 template <typename MathT, typename IdxT, typename LabelT>
 bool use_fused(IdxT m, IdxT n, IdxT k) {
@@ -218,8 +119,7 @@ inline std::enable_if_t<std::is_floating_point_v<MathT>> predict_core(
   switch (params.metric) {
     case cuvs::distance::DistanceType::L2Expanded:
     case cuvs::distance::DistanceType::L2SqrtExpanded: {
-      //size_t workspace_size = should_use_fused ? sizeof(int) * n_rows : sizeof(MathT) * n_rows * n_clusters;
-      size_t workspace_size = sizeof(MathT) * n_rows * n_clusters;
+      size_t workspace_size = should_use_fused ? sizeof(IdxT) * n_rows : sizeof(MathT) * n_rows * n_clusters;
       auto workspace = raft::make_device_mdarray<char, IdxT>(
         handle, mr, raft::make_extents<IdxT>(workspace_size));
 
@@ -241,7 +141,7 @@ inline std::enable_if_t<std::is_floating_point_v<MathT>> predict_core(
       raft::linalg::rowNorm<raft::linalg::L2Norm, true, MathT, IdxT>(
         centroidsNorm.data_handle(), centers, dim, n_clusters, stream);
 
-      //if (should_use_fused) {
+      if (should_use_fused) {
         cuvs::distance::fusedDistanceNNMinReduce<MathT, raft::KeyValuePair<IdxT, MathT>, IdxT>(
           minClusterAndDistance.data_handle(),
           dataset,
@@ -258,7 +158,7 @@ inline std::enable_if_t<std::is_floating_point_v<MathT>> predict_core(
           params.metric,
           0.0f,
           stream);
-      //} else {
+      } else {
         cuvs::distance::unfusedDistanceNNMinReduce<MathT, raft::KeyValuePair<IdxT, MathT>, IdxT>(
           unf_minClusterAndDistance.data_handle(),
           dataset,
@@ -276,14 +176,8 @@ inline std::enable_if_t<std::is_floating_point_v<MathT>> predict_core(
           0.0f,
           stream,
           cublas_h);
-      //}
+      }
 
-      ComparisonSummary* global_summary;
-      cudaMallocManaged(&global_summary, sizeof(ComparisonSummary));
-      global_summary->init();
-      vector_compare(global_summary, minClusterAndDistance.data_handle(), unf_minClusterAndDistance.data_handle(),
-          n_rows, stream);
-      global_summary->print();
       // todo(lsugy): use KVP + iterator in caller.
       // Copy keys to output labels
       thrust::transform(raft::resource::get_thrust_policy(handle),
