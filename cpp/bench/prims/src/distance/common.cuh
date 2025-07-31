562091d1908d99f5b5ac2eae491fea06b5893a02
Vinay D <vinayd@nvidia.com>
2025-07-31 20:57:59 +0100

    Pre-commit related changes

--- cpp/bench/prims/src/distance/common.cuh
+++ cpp/bench/prims/src/distance/common.cuh
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2025, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #pragma once
 
 #define LIBCUDACXX_ENABLE_EXPERIMENTAL_MEMORY_RESOURCE
@@ -283,7 +299,7 @@ void ref_l2nn_api(
 class ComparisonSummary {
  public:
   double max_diff;          // Maximum difference found
-  uint64_t max_diff_index;  // where does the maximim difference occur
+  uint64_t max_diff_index;  // where does the maximum difference occur
   double max_a;
   double max_b;
   double acc_diff;  // sum of all the diffs
