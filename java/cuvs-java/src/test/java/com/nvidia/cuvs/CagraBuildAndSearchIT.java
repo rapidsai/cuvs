55e3974c8009c41a40d080421350deb80dae3cfb
MithunR <mithunr@nvidia.com>
2025-08-06 13:39:05 -0700

    Reduce noise in Java tests
    When the Java tests run currently, they produce a large amount of logs
    to the console.  This includes contents of the test datasets, and
    various messages that ought to be trace/debug messages.
    
    This commit changes a lot of these logs to trace/debug messages, to
    reduce the console log noise.
    
    Signed-off-by: MithunR <mithunr@nvidia.com>

--- java/cuvs-java/src/test/java/com/nvidia/cuvs/CagraBuildAndSearchIT.java
+++ java/cuvs-java/src/test/java/com/nvidia/cuvs/CagraBuildAndSearchIT.java
@@ -62,7 +62,7 @@ public class CagraBuildAndSearchIT extends CuVSTestCase {
   public void setup() {
     assumeTrue("not supported on " + System.getProperty("os.name"), isLinuxAmd64());
     initializeRandom();
-    log.info("Random context initialized for test.");
+    log.trace("Random context initialized for test.");
   }
 
   private static void runConcurrently(int nThreads, Supplier<Runnable> runnableSupplier)
@@ -379,7 +379,7 @@ public class CagraBuildAndSearchIT extends CuVSTestCase {
 
       SearchResults fullSearchResults = index.search(fullQuery);
       List<Map<Integer, Float>> fullResults = fullSearchResults.getResults();
-      log.info("Full results: {}", fullResults);
+      log.debug("Full results: {}", fullResults);
 
       // Apply prefilter: only allow ids 0 and 2 (bitset: 1100)
       BitSet prefilter = new BitSet(4);
@@ -396,7 +396,7 @@ public class CagraBuildAndSearchIT extends CuVSTestCase {
 
       SearchResults filteredSearchResults = index.search(filteredQuery);
       List<Map<Integer, Float>> filteredResults = filteredSearchResults.getResults();
-      log.info("Filtered results: {}", filteredResults);
+      log.debug("Filtered results: {}", filteredResults);
 
       assertEquals(expectedResults, fullResults);
       assertEquals(expectedFilteredResults, filteredResults);
@@ -461,14 +461,14 @@ public class CagraBuildAndSearchIT extends CuVSTestCase {
     SearchResults results = index1.search(cuvsQuery);
 
     // Check results
-    log.info(results.getResults().toString());
+    log.debug(results.getResults().toString());
     checkResults(expectedResults, results.getResults());
 
     // Search from the second index
     results = index2.search(cuvsQuery);
 
     // Check results
-    log.info(results.getResults().toString());
+    log.debug(results.getResults().toString());
     checkResults(expectedResults, results.getResults());
   }
 
@@ -554,23 +554,23 @@ public class CagraBuildAndSearchIT extends CuVSTestCase {
               .withMetric(CuvsDistanceType.L2Expanded)
               .build();
 
-      log.info("Building first index...");
+      log.trace("Building first index...");
       CagraIndex index1 =
           CagraIndex.newBuilder(resources)
               .withDataset(vector1)
               .withIndexParams(indexParams)
               .build();
 
-      log.info("Building second index...");
+      log.trace("Building second index...");
       CagraIndex index2 =
           CagraIndex.newBuilder(resources)
               .withDataset(vector2)
               .withIndexParams(indexParams)
               .build();
 
-      log.info("Merging indexes...");
+      log.trace("Merging indexes...");
       CagraIndex mergedIndex = CagraIndex.merge(new CagraIndex[] {index1, index2});
-      log.info("Merge completed successfully");
+      log.trace("Merge completed successfully");
 
       CagraSearchParams searchParams = new CagraSearchParams.Builder().build();
 
@@ -582,9 +582,9 @@ public class CagraBuildAndSearchIT extends CuVSTestCase {
               .withMapping(SearchResults.IDENTITY_MAPPING)
               .build();
 
-      log.info("Searching merged index...");
+      log.trace("Searching merged index...");
       SearchResults results = mergedIndex.search(query);
-      log.info("Search results: " + results.getResults().toString());
+      log.debug("Search results: " + results.getResults().toString());
 
       assertEquals(expectedResults, results.getResults());
 
@@ -644,14 +644,14 @@ public class CagraBuildAndSearchIT extends CuVSTestCase {
               .withMetric(CuvsDistanceType.L2Expanded)
               .build();
 
-      log.info("Building first index...");
+      log.trace("Building first index...");
       CagraIndex index1 =
           CagraIndex.newBuilder(resources)
               .withDataset(vector1)
               .withIndexParams(indexParams)
               .build();
 
-      log.info("Building second index...");
+      log.trace("Building second index...");
       CagraIndex index2 =
           CagraIndex.newBuilder(resources)
               .withDataset(vector2)
@@ -673,10 +673,10 @@ public class CagraBuildAndSearchIT extends CuVSTestCase {
               .withStrategy(MergeStrategy.PHYSICAL)
               .build();
 
-      log.info("Merging indexes with PHYSICAL strategy...");
+      log.trace("Merging indexes with PHYSICAL strategy...");
       CagraIndex physicalMergedIndex =
           CagraIndex.merge(new CagraIndex[] {index1, index2}, physicalMergeParams);
-      log.info("Physical merge completed successfully");
+      log.trace("Physical merge completed successfully");
 
       CagraSearchParams searchParams = new CagraSearchParams.Builder().build();
 
@@ -688,7 +688,7 @@ public class CagraBuildAndSearchIT extends CuVSTestCase {
               .withMapping(SearchResults.IDENTITY_MAPPING)
               .build();
 
-      log.info("Searching physically merged index...");
+      log.trace("Searching physically merged index...");
       SearchResults physicalResults = physicalMergedIndex.search(query);
       assertNotNull("Physical merge search results should not be null", physicalResults);
       assertEquals(
