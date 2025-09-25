d4d2cff1fc17655612a1a3727e24be4e1e41ac33
Tarang Jain <jaintarang2015@gmail.com>
2025-09-25 14:54:52 -0700

    style

--- cpp/src/neighbors/ivf_pq/ivf_pq_build.cuh
+++ cpp/src/neighbors/ivf_pq/ivf_pq_build.cuh
@@ -157,8 +157,10 @@ void flat_compute_residuals(
   auto tmp_view = raft::make_device_vector_view<float, size_t>(tmp.data(), tmp.size());
 
   if (metric == cuvs::distance::DistanceType::CosineExpanded) {
-    raft::linalg::map(handle, tmp_view, raft::cast_op<float>{}, raft::make_device_vector_view<const T, IdxT>(dataset, n_rows * dim));
-    });
+    raft::linalg::map(handle,
+                      tmp_view,
+                      raft::cast_op<float>{},
+                      raft::make_device_vector_view<const T, IdxT>(dataset, n_rows * dim));
     auto tmp_matrix_view = raft::make_device_matrix_view<float, size_t>(tmp.data(), n_rows, dim);
     raft::linalg::row_normalize<raft::linalg::L2Norm>(
       handle, raft::make_const_mdspan(tmp_matrix_view), tmp_matrix_view);
