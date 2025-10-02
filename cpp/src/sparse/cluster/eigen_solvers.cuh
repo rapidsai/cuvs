bbee840a16dfe493107de4cb7482754ada87b2d3
Anupam <54245698+aamijar@users.noreply.github.com>
2025-10-02 09:47:53 -0700

    Move eigen_solvers from raft (#1402)
    Resolves #1344
    
    Authors:
      - Anupam (https://github.com/aamijar)
    
    Approvers:
      - Corey J. Nolet (https://github.com/cjnolet)
    
    URL: https://github.com/rapidsai/cuvs/pull/1402

--- cpp/src/sparse/cluster/eigen_solvers.cuh
+++ cpp/src/sparse/cluster/eigen_solvers.cuh
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019-2023, NVIDIA CORPORATION.
+ * Copyright (c) 2019-2025, NVIDIA CORPORATION.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -39,6 +39,8 @@ struct eigen_solver_config_t {
     1234567};  // CAVEAT: this default value is now common to all instances of using seed in
                // Lanczos; was not the case before: there were places where a default seed = 123456
                // was used; this may trigger slightly different # solver iterations
+
+  raft::sparse::solver::LANCZOS_WHICH which{raft::sparse::solver::LANCZOS_WHICH::SA};
 };
 
 template <typename index_type_t, typename value_type_t, typename size_type_t = index_type_t>
@@ -49,27 +51,57 @@ struct lanczos_solver_t {
   {
   }
 
-  index_type_t solve_smallest_eigenvectors(
+  auto solve_smallest_eigenvectors(
     raft::resources const& handle,
     raft::spectral::matrix::sparse_matrix_t<index_type_t, value_type_t, size_type_t> const& A,
     value_type_t* __restrict__ eigVals,
     value_type_t* __restrict__ eigVecs) const
   {
+    auto csr_structure =
+      raft::make_device_compressed_structure_view<index_type_t, index_type_t, index_type_t>(
+        const_cast<index_type_t*>(A.row_offsets_),
+        const_cast<index_type_t*>(A.col_indices_),
+        A.nrows_,
+        A.ncols_,
+        A.nnz_);
+
+    auto csr_matrix =
+      raft::make_device_csr_matrix_view<value_type_t, index_type_t, index_type_t, index_type_t>(
+        const_cast<value_type_t*>(A.values_), csr_structure);
+    return solve_smallest_eigenvectors(handle, csr_matrix, eigVals, eigVecs);
+  }
+
+  auto solve_smallest_eigenvectors(
+    raft::resources const& handle,
+    raft::device_csr_matrix_view<value_type_t, index_type_t, index_type_t, index_type_t> input,
+    value_type_t* __restrict__ eigVals,
+    value_type_t* __restrict__ eigVecs) const
+  {
     RAFT_EXPECTS(eigVals != nullptr, "Null eigVals buffer.");
     RAFT_EXPECTS(eigVecs != nullptr, "Null eigVecs buffer.");
-    index_type_t iters{};
-    raft::sparse::solver::computeSmallestEigenvectors(handle,
-                                                      A,
-                                                      config_.n_eigVecs,
-                                                      config_.maxIter,
-                                                      config_.restartIter,
-                                                      config_.tol,
-                                                      config_.reorthogonalize,
-                                                      iters,
-                                                      eigVals,
-                                                      eigVecs,
-                                                      config_.seed);
-    return iters;
+
+    auto lanczos_config =
+      raft::sparse::solver::lanczos_solver_config<value_type_t>{config_.n_eigVecs,
+                                                                config_.maxIter,
+                                                                config_.restartIter,
+                                                                config_.tol,
+                                                                config_.which,
+                                                                config_.seed};
+    auto v0_opt = std::optional<raft::device_vector_view<value_type_t, uint32_t, raft::row_major>>{
+      std::nullopt};
+    auto input_structure = input.structure_view();
+
+    auto solver_iterations = raft::sparse::solver::lanczos_compute_smallest_eigenvectors(
+      handle,
+      lanczos_config,
+      input,
+      v0_opt,
+      raft::make_device_vector_view<value_type_t, uint32_t, raft::col_major>(eigVals,
+                                                                             config_.n_eigVecs),
+      raft::make_device_matrix_view<value_type_t, uint32_t, raft::col_major>(
+        eigVecs, input_structure.get_n_rows(), config_.n_eigVecs));
+
+    return index_type_t{solver_iterations};
   }
 
   index_type_t solve_largest_eigenvectors(
