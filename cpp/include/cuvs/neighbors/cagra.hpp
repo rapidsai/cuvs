3101c5bae1cf15175b7868a006c004e394e41491
Julian Miller <julianmiller@nvidia.com>
2025-10-06 17:23:31 +0200

    Fix merge conflict

--- cpp/include/cuvs/neighbors/cagra.hpp
+++ cpp/include/cuvs/neighbors/cagra.hpp
@@ -350,14 +350,6 @@ struct index : cuvs::neighbors::index {
     return graph_view_;
   }
 
-  /** Dataset norms for cosine distance [size] */
-  [[nodiscard]] inline auto dataset_norms() const noexcept
-    -> std::optional<raft::device_vector_view<const float, int64_t>>
-  {
-    if (dataset_norms_.has_value()) { return raft::make_const_mdspan(dataset_norms_->view()); }
-    return std::nullopt;
-  }
-
   /** Whether the index is stored on disk */
   [[nodiscard]] constexpr inline auto on_disk() const noexcept -> bool { return on_disk_; }
 
@@ -613,10 +605,6 @@ struct index : cuvs::neighbors::index {
   void compute_dataset_norms_(raft::resources const& res);
   bool on_disk_               = false;
   std::string file_directory_ = "";
-  // only float distances supported at the moment
-  std::optional<raft::device_vector<float, int64_t>> dataset_norms_;
-
-  void compute_dataset_norms_(raft::resources const& res);
 };
 /**
  * @}
