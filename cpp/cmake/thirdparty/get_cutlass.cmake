2c0884ef835164e41b0efd271eacfd817aca073d
Robert Maynard <rmaynard@nvidia.com>
2025-10-13 09:54:37 -0400

    Remove cutlass build export as well

--- cpp/cmake/thirdparty/get_cutlass.cmake
+++ cpp/cmake/thirdparty/get_cutlass.cmake
@@ -58,27 +58,6 @@ function(find_and_configure_cutlass)
     add_library(nvidia::cutlass::cutlass ALIAS CUTLASS)
   endif()
 
-  if(NvidiaCutlass_ADDED)
-    rapids_export(
-      BUILD NvidiaCutlass
-      EXPORT_SET NvidiaCutlass
-      GLOBAL_TARGETS nvidia::cutlass::cutlass
-      NAMESPACE nvidia::cutlass::
-    )
-  endif()
-
-  # We generate the cutlass-config files when we built cutlass locally, so always do
-  # `find_dependency`
-  rapids_export_package(
-          BUILD NvidiaCutlass cuvs-exports GLOBAL_TARGETS nvidia::cutlass::cutlass
-  )
-
-  # Tell cmake where it can find the generated NvidiaCutlass-config.cmake we wrote.
-  include("${rapids-cmake-dir}/export/find_package_root.cmake")
-  rapids_export_find_package_root(
-          BUILD NvidiaCutlass [=[${CMAKE_CURRENT_LIST_DIR}]=]
-          EXPORT_SET cuvs-exports
-  )
 endfunction()
 
 find_and_configure_cutlass()
