9bf5a442e60180171fde427af0526cb7b6540423
Bradley Dice <bdice@bradleydice.com>
2025-10-16 01:35:34 -0500

    Refactor rapids_cpm_package_details to rapids_cpm_package_info
    Replace deprecated rapids_cpm_package_details function calls with the new rapids_cpm_package_info API across all third-party dependency cmake files.

--- cpp/cmake/thirdparty/get_hnswlib.cmake
+++ cpp/cmake/thirdparty/get_hnswlib.cmake
@@ -1,5 +1,5 @@
 #=============================================================================
-# Copyright (c) 2023-2024, NVIDIA CORPORATION.
+# Copyright (c) 2023-2025, NVIDIA CORPORATION.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -22,20 +22,20 @@ function(find_and_configure_hnswlib)
   set(patch_dir "${CMAKE_CURRENT_FUNCTION_LIST_DIR}/../patches")
   rapids_cpm_package_override("${patch_dir}/hnswlib_override.json")
 
-  include("${rapids-cmake-dir}/cpm/detail/package_details.cmake")
-  rapids_cpm_package_details(hnswlib version repository tag shallow exclude)
-
-  include("${rapids-cmake-dir}/cpm/detail/generate_patch_command.cmake")
-  rapids_cpm_generate_patch_command(hnswlib ${version} patch_command build_patch_only)
+  include("${rapids-cmake-dir}/cpm/detail/package_info.cmake")
+  rapids_cpm_package_info(hnswlib
+    VERSION_VAR version
+    FIND_VAR find_args
+    CPM_VAR cpm_args
+    TO_INSTALL_VAR to_install
+    BUILD_EXPORT_SET hnswlib-exports
+    INSTALL_EXPORT_SET hnswlib-exports
+  )
 
   rapids_cpm_find(
-    hnswlib ${version} ${build_patch_only}
+    hnswlib ${version} ${find_args}
     GLOBAL_TARGETS hnswlib hnswlib::hnswlib
-    CPM_ARGS
-    GIT_REPOSITORY ${repository}
-    GIT_TAG ${tag}
-    GIT_SHALLOW ${shallow} ${patch_command}
-    EXCLUDE_FROM_ALL ${exclude}
+    CPM_ARGS ${cpm_args}
     DOWNLOAD_ONLY ON
   )
 
@@ -53,7 +53,7 @@ function(find_and_configure_hnswlib)
   if(hnswlib_ADDED)
     # write build export rules
     install(TARGETS hnswlib EXPORT hnswlib-exports)
-    if(NOT exclude)
+    if(to_install)
       install(DIRECTORY "${hnswlib_SOURCE_DIR}/hnswlib/" DESTINATION include/hnswlib)
 
       # write install export rules
