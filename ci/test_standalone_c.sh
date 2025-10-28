432e394c92d5d443e29758970af665c4bf122801
Robert Maynard <rmaynard@nvidia.com>
2025-10-28 16:34:25 -0400

    Fixing test_standalone_c.sh

--- ci/test_standalone_c.sh
+++ ci/test_standalone_c.sh
@@ -8,7 +8,7 @@ set -euo pipefail
 pkg_name="libcuvs_c_${RAPIDS_CUDA_VERSION}.tar.gz.zip"
 rapids-logger "Download artifacts from previous jobs"
 rapids-logger "pkg_name ${pkg_name}"
-rapids-download-from-github rapids-package-name "${pkg_name}"
+rapids-download-from-github "${pkg_name}"
 
 # Extract the artifact to a staging directory
 INSTALL_PREFIX="${PWD}/libcuvs_c_install"
