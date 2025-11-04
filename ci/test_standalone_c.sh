a0106c17e5fb6fb9f157cc55f3513811e0b5a171
Robert Maynard <rmaynard@nvidia.com>
2025-11-04 16:19:32 -0500

    Handle the fact that the artifact is a zip

--- ci/test_standalone_c.sh
+++ ci/test_standalone_c.sh
@@ -6,14 +6,16 @@ set -euo pipefail
 
 
 # Download the standalone C library artifact
+pkg_key="libcuvs_c_${RAPIDS_CUDA_VERSION}.tar.gz"
 pkg_name="libcuvs_c_${RAPIDS_CUDA_VERSION}.tar.gz"
 rapids-logger "Download artifacts from previous jobs"
-rapids-logger "pkg_name ${pkg_name}"
-rapids-download-from-github "${pkg_name}"
+rapids-logger "pkg_key ${pkg_key}"
+rapids-download-from-github "${pkg_key}"
 
 # Extract the artifact to a staging directory
 INSTALL_PREFIX="${PWD}/libcuvs_c_install"
 mkdir -p "${INSTALL_PREFIX}"
+unzip "${pkg_name}.zip"
 tar -xzf "${pkg_name}" -C "${INSTALL_PREFIX}"
 
 rapids-logger "Run C API tests"
