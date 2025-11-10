fc21b56d9775d8927934b33bb4f9c697d3304fad
Divye Gala <divyegala@gmail.com>
2025-11-10 20:28:14 +0000

    one more extern

--- cpp/src/cluster/kmeans_balanced.cuh
+++ cpp/src/cluster/kmeans_balanced.cuh
@@ -204,29 +204,25 @@ void build_clusters(const raft::resources& handle,
                     raft::device_vector_view<LabelT, IndexT> labels,
                     raft::device_vector_view<CounterT, IndexT> cluster_sizes,
                     MappingOpT mapping_op = raft::identity_op(),
-                    std::optional<raft::device_vector_view<const MathT>> X_norm = std::nullopt)
-{
-  RAFT_EXPECTS(X.extent(0) == labels.extent(0),
-               "Number of rows in dataset and labels are different");
-  RAFT_EXPECTS(X.extent(1) == centroids.extent(1),
-               "Number of features in dataset and centroids are different");
-  RAFT_EXPECTS(centroids.extent(0) == cluster_sizes.extent(0),
-               "Number of rows in centroids and clusyer_sizes are different");
+                    std::optional<raft::device_vector_view<const MathT>> X_norm = std::nullopt);
 
-  cuvs::cluster::kmeans::detail::build_clusters(
-    handle,
-    params,
-    X.extent(1),
-    X.data_handle(),
-    X.extent(0),
-    centroids.extent(0),
-    centroids.data_handle(),
-    labels.data_handle(),
-    cluster_sizes.data_handle(),
-    mapping_op,
-    raft::resource::get_workspace_resource(handle),
-    X_norm.has_value() ? X_norm.value().data_handle() : nullptr);
-}
+#define EXTERN_TEMPLATE_BUILD_CLUSTERS(DataT, MathT, IndexT, LabelT, CounterT, MappingOpT) \
+  extern template void build_clusters<DataT, MathT, IndexT, LabelT, CounterT, MappingOpT>( \
+    const raft::resources& handle,                                                         \
+    const cuvs::cluster::kmeans::balanced_params& params,                                  \
+    raft::device_matrix_view<const DataT, IndexT> X,                                       \
+    raft::device_matrix_view<MathT, IndexT> centroids,                                     \
+    raft::device_vector_view<LabelT, IndexT> labels,                                       \
+    raft::device_vector_view<CounterT, IndexT> cluster_sizes,                              \
+    MappingOpT mapping_op,                                                                 \
+    std::optional<raft::device_vector_view<const MathT>> X_norm);
+
+// Extern template declaration for the instantiation actually used in IVF-PQ build
+// IVF-PQ converts input data to float before calling build_clusters
+EXTERN_TEMPLATE_BUILD_CLUSTERS(
+  float, float, int64_t, uint32_t, uint32_t, cuvs::spatial::knn::detail::utils::mapping<float>)
+
+#undef EXTERN_TEMPLATE_BUILD_CLUSTERS
 
 /**
  * @brief Given the data and labels, calculate cluster centers and sizes in one sweep.
