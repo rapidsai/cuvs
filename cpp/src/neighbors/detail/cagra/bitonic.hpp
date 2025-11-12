178459a6d83cc97543e733c32b0d3b3a92137f3b
Seunghwa Kang <seunghwak@nvidia.com>
2025-11-12 09:54:28 -0800

    undo most of the changes in bitonic.hpp except for the less unrolling on successive warp_merge calls when N is large, we now handle run-time branch in a higher level

--- cpp/src/neighbors/detail/cagra/bitonic.hpp
+++ cpp/src/neighbors/detail/cagra/bitonic.hpp
@@ -41,52 +41,139 @@ RAFT_DEVICE_INLINE_FUNCTION void swap_if_needed(K& k0,
   }
 }
 
-template <class K, class V, unsigned warp_size = 32>
-struct warp_merge_core_n {
-  RAFT_DEVICE_INLINE_FUNCTION void operator()(
-    K* ks, V* vs, unsigned n, const std::uint32_t range, const bool asc)
+template <class K, class V, unsigned N, unsigned warp_size = 32>
+struct warp_merge_core {
+  RAFT_DEVICE_INLINE_FUNCTION void operator()(K k[N],
+                                              V v[N],
+                                              const std::uint32_t range,
+                                              const bool asc)
   {
     const auto lane_id = threadIdx.x % warp_size;
 
     if (range == 1) {
-      for (std::uint32_t b = 2; b <= n; b <<= 1) {
+      for (std::uint32_t b = 2; b <= N; b <<= 1) {
         for (std::uint32_t c = b / 2; c >= 1; c >>= 1) {
 #pragma unroll
-          for (std::uint32_t i = 0; i < n; i++) {
+          for (std::uint32_t i = 0; i < N; i++) {
             std::uint32_t j = i ^ c;
             if (i >= j) continue;
-            const auto line_id = i + (n * lane_id);
+            const auto line_id = i + (N * lane_id);
             const auto p       = static_cast<bool>(line_id & b) == static_cast<bool>(line_id & c);
-            swap_if_needed(ks[i], vs[i], ks[j], vs[j], p);
+            swap_if_needed(k[i], v[i], k[j], v[j], p);
           }
         }
       }
-    } else {
-      const std::uint32_t b = range;
-      for (std::uint32_t c = b / 2; c >= 1; c >>= 1) {
-        const auto p = static_cast<bool>(lane_id & b) == static_cast<bool>(lane_id & c);
+      return;
+    }
+
+    const std::uint32_t b = range;
+    for (std::uint32_t c = b / 2; c >= 1; c >>= 1) {
+      const auto p = static_cast<bool>(lane_id & b) == static_cast<bool>(lane_id & c);
 #pragma unroll
-        for (std::uint32_t i = 0; i < n; i++) {
-          swap_if_needed(ks[i], vs[i], c, p);
-        }
+      for (std::uint32_t i = 0; i < N; i++) {
+        swap_if_needed(k[i], v[i], c, p);
       }
-      const auto p = ((lane_id & b) == 0);
-      for (std::uint32_t c = n / 2; c >= 1; c >>= 1) {
+    }
+    const auto p = ((lane_id & b) == 0);
+    for (std::uint32_t c = N / 2; c >= 1; c >>= 1) {
 #pragma unroll
-        for (std::uint32_t i = 0; i < n; i++) {
-          std::uint32_t j = i ^ c;
-          if (i >= j) continue;
-          swap_if_needed(ks[i], vs[i], ks[j], vs[j], p);
-        }
+      for (std::uint32_t i = 0; i < N; i++) {
+        std::uint32_t j = i ^ c;
+        if (i >= j) continue;
+        swap_if_needed(k[i], v[i], k[j], v[j], p);
       }
     }
   }
 };
 
 template <class K, class V, unsigned warp_size>
-struct warp_merge_core_2 {
-  RAFT_DEVICE_INLINE_FUNCTION void operator()(K* ks,
-                                              V* vs,
+struct warp_merge_core<K, V, 6, warp_size> {
+  RAFT_DEVICE_INLINE_FUNCTION void operator()(K k[6],
+                                              V v[6],
+                                              const std::uint32_t range,
+                                              const bool asc)
+  {
+    constexpr unsigned N = 6;
+    const auto lane_id   = threadIdx.x % warp_size;
+
+    if (range == 1) {
+      for (std::uint32_t i = 0; i < N; i += 3) {
+        const auto p = (i == 0);
+        swap_if_needed(k[0 + i], v[0 + i], k[1 + i], v[1 + i], p);
+        swap_if_needed(k[1 + i], v[1 + i], k[2 + i], v[2 + i], p);
+        swap_if_needed(k[0 + i], v[0 + i], k[1 + i], v[1 + i], p);
+      }
+      const auto p = ((lane_id & 1) == 0);
+      for (std::uint32_t i = 0; i < 3; i++) {
+        std::uint32_t j = i + 3;
+        swap_if_needed(k[i], v[i], k[j], v[j], p);
+      }
+      for (std::uint32_t i = 0; i < N; i += 3) {
+        swap_if_needed(k[0 + i], v[0 + i], k[1 + i], v[1 + i], p);
+        swap_if_needed(k[1 + i], v[1 + i], k[2 + i], v[2 + i], p);
+        swap_if_needed(k[0 + i], v[0 + i], k[1 + i], v[1 + i], p);
+      }
+      return;
+    }
+
+    const std::uint32_t b = range;
+    for (std::uint32_t c = b / 2; c >= 1; c >>= 1) {
+      const auto p = static_cast<bool>(lane_id & b) == static_cast<bool>(lane_id & c);
+#pragma unroll
+      for (std::uint32_t i = 0; i < N; i++) {
+        swap_if_needed(k[i], v[i], c, p);
+      }
+    }
+    const auto p = ((lane_id & b) == 0);
+    for (std::uint32_t i = 0; i < 3; i++) {
+      std::uint32_t j = i + 3;
+      swap_if_needed(k[i], v[i], k[j], v[j], p);
+    }
+    for (std::uint32_t i = 0; i < N; i += N / 2) {
+      swap_if_needed(k[0 + i], v[0 + i], k[1 + i], v[1 + i], p);
+      swap_if_needed(k[1 + i], v[1 + i], k[2 + i], v[2 + i], p);
+      swap_if_needed(k[0 + i], v[0 + i], k[1 + i], v[1 + i], p);
+    }
+  }
+};
+
+template <class K, class V, unsigned warp_size>
+struct warp_merge_core<K, V, 3, warp_size> {
+  RAFT_DEVICE_INLINE_FUNCTION void operator()(K k[3],
+                                              V v[3],
+                                              const std::uint32_t range,
+                                              const bool asc)
+  {
+    constexpr unsigned N = 3;
+    const auto lane_id   = threadIdx.x % warp_size;
+
+    if (range == 1) {
+      const auto p = ((lane_id & 1) == 0);
+      swap_if_needed(k[0], v[0], k[1], v[1], p);
+      swap_if_needed(k[1], v[1], k[2], v[2], p);
+      swap_if_needed(k[0], v[0], k[1], v[1], p);
+      return;
+    }
+
+    const std::uint32_t b = range;
+    for (std::uint32_t c = b / 2; c >= 1; c >>= 1) {
+      const auto p = static_cast<bool>(lane_id & b) == static_cast<bool>(lane_id & c);
+#pragma unroll
+      for (std::uint32_t i = 0; i < N; i++) {
+        swap_if_needed(k[i], v[i], c, p);
+      }
+    }
+    const auto p = ((lane_id & b) == 0);
+    swap_if_needed(k[0], v[0], k[1], v[1], p);
+    swap_if_needed(k[1], v[1], k[2], v[2], p);
+    swap_if_needed(k[0], v[0], k[1], v[1], p);
+  }
+};
+
+template <class K, class V, unsigned warp_size>
+struct warp_merge_core<K, V, 2, warp_size> {
+  RAFT_DEVICE_INLINE_FUNCTION void operator()(K k[2],
+                                              V v[2],
                                               const std::uint32_t range,
                                               const bool asc)
   {
@@ -95,26 +182,27 @@ struct warp_merge_core_2 {
 
     if (range == 1) {
       const auto p = ((lane_id & 1) == 0);
-      swap_if_needed(ks[0], vs[0], ks[1], vs[1], p);
-    } else {
-      const std::uint32_t b = range;
-      for (std::uint32_t c = b / 2; c >= 1; c >>= 1) {
-        const auto p = static_cast<bool>(lane_id & b) == static_cast<bool>(lane_id & c);
-#pragma unroll
-        for (std::uint32_t i = 0; i < N; i++) {
-          swap_if_needed(ks[i], vs[i], c, p);
-        }
-      }
-      const auto p = ((lane_id & b) == 0);
-      swap_if_needed(ks[0], vs[0], ks[1], vs[1], p);
+      swap_if_needed(k[0], v[0], k[1], v[1], p);
+      return;
     }
+
+    const std::uint32_t b = range;
+    for (std::uint32_t c = b / 2; c >= 1; c >>= 1) {
+      const auto p = static_cast<bool>(lane_id & b) == static_cast<bool>(lane_id & c);
+#pragma unroll
+      for (std::uint32_t i = 0; i < N; i++) {
+        swap_if_needed(k[i], v[i], c, p);
+      }
+    }
+    const auto p = ((lane_id & b) == 0);
+    swap_if_needed(k[0], v[0], k[1], v[1], p);
   }
 };
 
 template <class K, class V, unsigned warp_size>
-struct warp_merge_core_1 {
-  RAFT_DEVICE_INLINE_FUNCTION void operator()(K* ks,
-                                              V* vs,
+struct warp_merge_core<K, V, 1, warp_size> {
+  RAFT_DEVICE_INLINE_FUNCTION void operator()(K k[1],
+                                              V v[1],
                                               const std::uint32_t range,
                                               const bool asc)
   {
@@ -122,32 +210,32 @@ struct warp_merge_core_1 {
     const std::uint32_t b = range;
     for (std::uint32_t c = b / 2; c >= 1; c >>= 1) {
       const auto p = static_cast<bool>(lane_id & b) == static_cast<bool>(lane_id & c);
-      swap_if_needed(ks[0], vs[0], c, p);
+      swap_if_needed(k[0], v[0], c, p);
     }
   }
 };
 
 }  // namespace detail
 
-template <class K, class V, unsigned warp_size = 32>
-RAFT_DEVICE_INLINE_FUNCTION void warp_merge(
-  K* ks, V* vs, unsigned n, unsigned range, const bool asc = true)
+template <class K, class V, unsigned N, unsigned warp_size = 32>
+RAFT_DEVICE_INLINE_FUNCTION void warp_merge(K k[N], V v[N], unsigned range, const bool asc = true)
 {
-  if (n == 1) {
-    detail::warp_merge_core_1<K, V, warp_size>{}(ks, vs, range, asc);
-  } else if (n == 2) {
-    detail::warp_merge_core_2<K, V, warp_size>{}(ks, vs, range, asc);
-  } else {
-    detail::warp_merge_core_n<K, V, warp_size>{}(ks, vs, n, range, asc);
-  }
+  detail::warp_merge_core<K, V, N, warp_size>{}(k, v, range, asc);
 }
 
-template <class K, class V, unsigned warp_size = 32>
-RAFT_DEVICE_INLINE_FUNCTION void warp_sort(K* ks, V* vs, unsigned n, const bool asc = true)
+template <class K, class V, unsigned N, unsigned warp_size = 32>
+RAFT_DEVICE_INLINE_FUNCTION void warp_sort(K k[N], V v[N], const bool asc = true)
 {
+  if constexpr (N < 8) {
+#pragma unroll
+    for (std::uint32_t range = 1; range <= warp_size; range <<= 1) {
+      warp_merge<K, V, N, warp_size>(k, v, range, asc);
+    }
+  } else {
 #pragma unroll 1
-  for (std::uint32_t range = 1; range <= warp_size; range <<= 1) {
-    warp_merge<K, V, warp_size>(ks, vs, n, range, asc);
+    for (std::uint32_t range = 1; range <= warp_size; range <<= 1) {
+      warp_merge<K, V, N, warp_size>(k, v, range, asc);
+    }
   }
 }
 
