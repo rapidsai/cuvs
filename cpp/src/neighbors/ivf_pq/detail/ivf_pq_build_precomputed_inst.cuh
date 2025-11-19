25a12a7282174510a561cb2d663b244cbc084f87
Tarang Jain <jaintarang2015@gmail.com>
2025-11-18 17:42:21 -0800

    cleanup diff

--- cpp/src/neighbors/ivf_pq/detail/ivf_pq_build_precomputed_inst.cuh
+++ cpp/src/neighbors/ivf_pq/detail/ivf_pq_build_precomputed_inst.cuh
@@ -11,32 +11,6 @@
 
 namespace cuvs::neighbors::ivf_pq {
 #define CUVS_INST_IVF_PQ_BUILD_PRECOMPUTED(IdxT)                                                   \
-  auto build_view(                                                                                 \
-    raft::resources const& handle,                                                                 \
-    const cuvs::neighbors::ivf_pq::index_params& index_params,                                     \
-    const uint32_t dim,                                                                            \
-    raft::device_mdspan<const float, raft::extent_3d<uint32_t>, raft::row_major> pq_centers,       \
-    raft::device_matrix_view<const float, uint32_t, raft::row_major> centers,                      \
-    raft::device_matrix_view<const float, uint32_t, raft::row_major> centers_rot,                  \
-    raft::device_matrix_view<const float, uint32_t, raft::row_major> rotation_matrix)              \
-    -> cuvs::neighbors::ivf_pq::index<IdxT>                                                        \
-  {                                                                                                \
-    return detail::build_view<IdxT>(                                                               \
-      handle, index_params, dim, pq_centers, centers, centers_rot, rotation_matrix);               \
-  }                                                                                                \
-  auto build_owning(                                                                               \
-    raft::resources const& handle,                                                                 \
-    const cuvs::neighbors::ivf_pq::index_params& index_params,                                     \
-    const uint32_t dim,                                                                            \
-    raft::device_mdspan<const float, raft::extent_3d<uint32_t>, raft::row_major> pq_centers,       \
-    raft::device_matrix_view<const float, uint32_t, raft::row_major> centers,                      \
-    std::optional<raft::device_matrix_view<const float, uint32_t, raft::row_major>> centers_rot,   \
-    std::optional<raft::device_matrix_view<const float, uint32_t, raft::row_major>>                \
-      rotation_matrix) -> cuvs::neighbors::ivf_pq::index<IdxT>                                     \
-  {                                                                                                \
-    return detail::build_owning<IdxT>(                                                             \
-      handle, index_params, dim, pq_centers, centers, centers_rot, rotation_matrix);               \
-  }                                                                                                \
   auto build(                                                                                      \
     raft::resources const& handle,                                                                 \
     const cuvs::neighbors::ivf_pq::index_params& index_params,                                     \
@@ -62,6 +36,25 @@ namespace cuvs::neighbors::ivf_pq {
   {                                                                                                \
     detail::build<IdxT>(                                                                           \
       handle, index_params, dim, pq_centers, centers, centers_rot, rotation_matrix, idx);          \
-  }
+  }                                                                                                \
+  template <typename pq_centers_accessor,                                                          \
+            typename centers_accessor,                                                             \
+            typename centers_rot_accessor,                                                         \
+            typename rotation_matrix_accessor,                                                     \
+            typename = std::enable_if_t<                                                           \
+              raft::is_device_mdspan_v<raft::mdspan<const float, raft::extent_3d<uint32_t>, raft::row_major, pq_centers_accessor>>>> \
+  auto build(                                                                                      \
+    raft::resources const& handle,                                                                 \
+    const cuvs::neighbors::ivf_pq::index_params& index_params,                                     \
+    const uint32_t dim,                                                                            \
+    raft::mdspan<const float, raft::extent_3d<uint32_t>, raft::row_major, pq_centers_accessor> pq_centers, \
+    raft::mdspan<const float, raft::matrix_extent<uint32_t>, raft::row_major, centers_accessor> centers, \
+    raft::mdspan<const float, raft::matrix_extent<uint32_t>, raft::row_major, centers_rot_accessor> centers_rot, \
+    raft::mdspan<const float, raft::matrix_extent<uint32_t>, raft::row_major, rotation_matrix_accessor> rotation_matrix) \
+    -> cuvs::neighbors::ivf_pq::index<IdxT>                                                        \
+  {                                                                                                \
+    return detail::build<IdxT>(                                                                    \
+      handle, index_params, dim, pq_centers, centers, centers_rot, rotation_matrix);               \
+  }                                                                                                \
 
 }  // namespace cuvs::neighbors::ivf_pq
