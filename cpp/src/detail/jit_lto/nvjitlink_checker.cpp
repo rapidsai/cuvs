e3b749d07413e82ad803d3bafcc655718046b1f2
Kyle Edwards <kyedwards@nvidia.com>
2025-12-15 15:24:13 +0000

    Fix nvjitlink_checker

---
+++ cpp/src/detail/jit_lto/nvjitlink_checker.cpp
@@ -0,0 +1,28 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2025, NVIDIA CORPORATION.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "nvjitlink_checker.hpp"
+
+#include <memory>
+#include <nvJitLink.h>
+#include <string>
+
+#include <raft/core/error.hpp>
+
+void check_nvjitlink_result(nvJitLinkHandle handle, nvJitLinkResult result)
+{
+  if (result != NVJITLINK_SUCCESS) {
+    RAFT_FAIL("nvJITLink failed with error %s", std::to_string(result).c_str());
+    size_t log_size = 0;
+    result          = nvJitLinkGetErrorLogSize(handle, &log_size);
+    if (result == NVJITLINK_SUCCESS && log_size > 0) {
+      std::unique_ptr<char[]> log{new char[log_size]};
+      result = nvJitLinkGetErrorLog(handle, log.get());
+      if (result == NVJITLINK_SUCCESS) {
+        RAFT_FAIL("AlgorithmPlanner nvJITLink error log: %s", std::string(log.get()).c_str());
+      }
+    }
+  }
+}
