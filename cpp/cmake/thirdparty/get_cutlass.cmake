9d102ce4543c8db38cbf5c4fd4f713ac64d32d77
James Lamb <jlamb@nvidia.com>
2026-01-09 09:46:05 -0600

    try removing cutlass from the install export set

--- cpp/cmake/thirdparty/get_cutlass.cmake
+++ cpp/cmake/thirdparty/get_cutlass.cmake
@@ -60,16 +60,9 @@ function(find_and_configure_cutlass)
   rapids_export_package(
       BUILD NvidiaCutlass cuvs-exports GLOBAL_TARGETS nvidia::cutlass::cutlass
   )
-  rapids_export_package(
-      INSTALL NvidiaCutlass cuvs-exports GLOBAL_TARGETS nvidia::cutlass::cutlass
-  )
 
   # Tell cmake where it can find the generated NvidiaCutlass-config.cmake we wrote.
   include("${rapids-cmake-dir}/export/find_package_root.cmake")
-  rapids_export_find_package_root(
-          INSTALL NvidiaCutlass [=[${CMAKE_CURRENT_LIST_DIR}/../]=]
-          EXPORT_SET cuvs-exports
-  )
   rapids_export_find_package_root(
           BUILD NvidiaCutlass [=[${CMAKE_CURRENT_LIST_DIR}]=]
           EXPORT_SET cuvs-exports
