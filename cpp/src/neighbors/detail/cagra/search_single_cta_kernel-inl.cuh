305cb5596756dcff0cef2b61514bbb4910a8728c
MithunR <mithunr@nvidia.com>
2026-02-04 10:56:00 -0800

    Copyright date. Formatting.
    Signed-off-by: MithunR <mithunr@nvidia.com>

--- cpp/src/neighbors/detail/cagra/search_single_cta_kernel-inl.cuh
+++ cpp/src/neighbors/detail/cagra/search_single_cta_kernel-inl.cuh
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: Copyright (c) 2023-2025, NVIDIA CORPORATION.
+ * SPDX-FileCopyrightText: Copyright (c) 2023-2026, NVIDIA CORPORATION.
  * SPDX-License-Identifier: Apache-2.0
  */
 #pragma once
@@ -2121,17 +2121,17 @@ auto get_runner(Args... args) -> std::shared_ptr<RunnerT>
  * @param smem_size The size of the dynamic shared memory to be set.
  * @param kernel The kernel to be set.
  */
-template <typename KernelT> // inline 
+template <typename KernelT>  // inline
 void optionally_set_larger_max_smem_size(uint32_t smem_size, KernelT& kernel)
 {
-  static auto mutex = std::mutex{};
+  static auto mutex                 = std::mutex{};
   static auto running_max_smem_size = uint32_t{0};
   if (smem_size > running_max_smem_size) {
     auto guard = std::lock_guard<std::mutex>{mutex};
     if (smem_size > running_max_smem_size) {
       running_max_smem_size = smem_size;
-      RAFT_CUDA_TRY(
-        cudaFuncSetAttribute(kernel, cudaFuncAttributeMaxDynamicSharedMemorySize, running_max_smem_size));
+      RAFT_CUDA_TRY(cudaFuncSetAttribute(
+        kernel, cudaFuncAttributeMaxDynamicSharedMemorySize, running_max_smem_size));
     }
   }
 }
