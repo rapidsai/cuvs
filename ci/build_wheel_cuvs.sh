2e6d3e7d46aa272a8f7ba890e80b7ba58b1917f7
Gil Forsyth <gforsyth@nvidia.com>
2026-02-05 12:12:08 -0500

    refactor: remove 3.10 branching, set py-api from command line

--- ci/build_wheel_cuvs.sh
+++ ci/build_wheel_cuvs.sh
@@ -6,11 +6,6 @@ set -euo pipefail
 
 source rapids-init-pip
 
-# Only use stable ABI package for Python >= 3.11
-if [[ "${RAPIDS_PY_VERSION}" != "3.10" ]]; then
-  source ./ci/use_upstream_sabi_wheels.sh
-fi
-
 package_dir="python/cuvs"
 
 RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen "${RAPIDS_CUDA_VERSION}")"
@@ -23,11 +18,13 @@ RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen "${RAPIDS_CUDA_VERSION}")"
 LIBCUVS_WHEELHOUSE=$(RAPIDS_PY_WHEEL_NAME="libcuvs_${RAPIDS_PY_CUDA_SUFFIX}" rapids-download-wheels-from-github cpp)
 echo "libcuvs-${RAPIDS_PY_CUDA_SUFFIX} @ file://$(echo "${LIBCUVS_WHEELHOUSE}"/libcuvs_*.whl)" >> "${PIP_CONSTRAINT}"
 
-ci/build_wheel.sh cuvs ${package_dir}
+# TODO: move this variable into `ci-wheel`
+# Format Python limited API version string
+RAPIDS_PY_API="cp${RAPIDS_PY_VERSION//./}"
+export RAPIDS_PY_API
+
+ci/build_wheel.sh cuvs ${package_dir} --stable
 ci/validate_wheel.sh ${package_dir} "${RAPIDS_WHEEL_BLD_OUTPUT_DIR}"
 
-# Only use stable ABI package naming for Python >= 3.11
-if [[ "${RAPIDS_PY_VERSION}" != "3.10" ]]; then
-  RAPIDS_PACKAGE_NAME="$(rapids-package-name wheel_python cuvs --stable --cuda)"
-  export RAPIDS_PACKAGE_NAME
-fi
+RAPIDS_PACKAGE_NAME="$(rapids-package-name wheel_python cuvs --stable --cuda)"
+export RAPIDS_PACKAGE_NAME
--- ci/test_wheel_cuvs.sh
+++ ci/test_wheel_cuvs.sh
@@ -11,13 +11,7 @@ rm -rf /usr/lib64/libnccl*
 
 RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen "${RAPIDS_CUDA_VERSION}")"
 LIBCUVS_WHEELHOUSE=$(RAPIDS_PY_WHEEL_NAME="libcuvs_${RAPIDS_PY_CUDA_SUFFIX}" rapids-download-wheels-from-github cpp)
-
-if [[ "${RAPIDS_PY_VERSION}" != "3.10" ]]; then
-    CUVS_WHEELHOUSE=$(rapids-download-from-github "$(rapids-package-name "wheel_python" cuvs --stable --cuda "$RAPIDS_CUDA_VERSION")")
-    source ./ci/use_upstream_sabi_wheels.sh
-else
-    CUVS_WHEELHOUSE=$(RAPIDS_PY_WHEEL_NAME="cuvs_${RAPIDS_PY_CUDA_SUFFIX}" rapids-download-wheels-from-github python)
-fi
+CUVS_WHEELHOUSE=$(rapids-download-from-github "$(rapids-package-name "wheel_python" cuvs --stable --cuda "$RAPIDS_CUDA_VERSION")")
 
 # echo to expand wildcard before adding `[extra]` requires for pip
 rapids-pip-retry install \
