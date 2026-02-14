da6136e10d44cf54998dcfd42f982ac8245f55c8
Divye Gala <divyegala@gmail.com>
2026-02-13 22:07:28 -0800

    JIT compile `interleaved_scan_kernel` for CUDA 13 (#1405)
    Closes #1520 
    
    This PR introduces infrastructure to generate LTO-IR representations of kernels at compile time. At runtime, this LTO-IR representations are JITted to SASS using the architecture of the native GPU with LTO (Link Time Optimizations) of device functions called within the kernel.
    
    The changes in this PR can be divided in 4 parts:
    1. CMake code to generate fatbins to JIT at runtime
    2. Architecture to handle JITing and caching of kernels
    3. Changes to the build system to bring in `nvJitLink` as a CUDA 13 dependency
    4. Using all of the above to JIT `interleaved_scan_kernel`, where we use a Python script to generate the files that are using for first compiling and then for embedding fatbins
    
    Binary sizes of `interleaved_scan_*.cu` TUs in CUDA 13:
    | TU name   | Size |
    |---|---|
    | ivf_flat_interleaved_scan_half_int64_t.cu.o  | 5.658 MB |
    | vf_flat_interleaved_scan_half_int64_t_bitset.cu.o | 5.998 MB |
    | ivf_flat_interleaved_scan_uint8_t_int64_t.cu.o | 6.402 MB |
    | ivf_flat_interleaved_scan_int8_t_int64_t.cu.o | 6.338 MB |
    | ivf_flat_interleaved_scan_float_int64_t_bitset.cu.o | 6.273 MB |
    | ivf_flat_interleaved_scan_float_int64_t.cu.o | 5.872 MB |
    | ivf_flat_interleaved_scan_uint8_t_int64_t_bitset.cu.o | 6.851 MB |
    | ivf_flat_interleaved_scan_int8_t_int64_t_bitset.cu.o | 6.901 MB |
    
    Total size of aforementioned TUs: **50.293 MB**
    Total size of `libcuvs.so` on 27/10/2025 in main **468.79 MB**
    Total size of `libcuvs.so` in this PR: **444.70 MB**
    
    Authors:
      - Divye Gala (https://github.com/divyegala)
      - Kyle Edwards (https://github.com/KyleFromNVIDIA)
    
    Approvers:
      - Kyle Edwards (https://github.com/KyleFromNVIDIA)
      - Dante Gama Dessavre (https://github.com/dantegd)
      - Micka (https://github.com/lowener)
      - Robert Maynard (https://github.com/robertmaynard)
    
    URL: https://github.com/rapidsai/cuvs/pull/1405

---
+++ cpp/src/detail/jit_lto/AlgorithmPlanner.cu
@@ -0,0 +1,120 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2025-2026, NVIDIA CORPORATION.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "nvjitlink_checker.hpp"
+
+#include <chrono>
+#include <iterator>
+#include <memory>
+#include <mutex>
+#include <new>
+#include <string>
+#include <vector>
+
+#include <cuvs/detail/jit_lto/AlgorithmPlanner.hpp>
+#include <cuvs/detail/jit_lto/FragmentDatabase.hpp>
+
+#include "cuda_runtime.h"
+#include "nvJitLink.h"
+
+#include <raft/core/logger.hpp>
+#include <raft/util/cuda_rt_essentials.hpp>
+
+void AlgorithmPlanner::add_entrypoint()
+{
+  auto entrypoint_fragment = fragment_database().get_fragment(this->entrypoint);
+  this->fragments.push_back(entrypoint_fragment);
+}
+
+void AlgorithmPlanner::add_device_functions()
+{
+  for (const auto& device_function_key : this->device_functions) {
+    auto device_function_fragment = fragment_database().get_fragment(device_function_key);
+    this->fragments.push_back(device_function_fragment);
+  }
+}
+
+std::string AlgorithmPlanner::get_device_functions_key() const
+{
+  std::string key = "";
+  for (const auto& device_function : this->device_functions) {
+    key += device_function;
+  }
+  return key;
+}
+
+std::shared_ptr<AlgorithmLauncher> AlgorithmPlanner::get_launcher()
+{
+  auto& launchers = get_cached_launchers();
+  auto launch_key = this->entrypoint + this->get_device_functions_key();
+
+  static std::mutex cache_mutex;
+  std::lock_guard<std::mutex> lock(cache_mutex);
+  if (launchers.count(launch_key) == 0) {
+    add_entrypoint();
+    add_device_functions();
+    std::string log_message =
+      "JIT compiling launcher for entrypoint: " + this->entrypoint + " and device functions: ";
+    for (const auto& device_function : this->device_functions) {
+      log_message += device_function + ",";
+    }
+    log_message.pop_back();
+    RAFT_LOG_INFO("%s", log_message.c_str());
+    launchers[launch_key] = this->build();
+  }
+  return launchers[launch_key];
+}
+
+std::shared_ptr<AlgorithmLauncher> AlgorithmPlanner::build()
+{
+  int device = 0;
+  int major  = 0;
+  int minor  = 0;
+  RAFT_CUDA_TRY(cudaGetDevice(&device));
+  RAFT_CUDA_TRY(cudaDeviceGetAttribute(&major, cudaDevAttrComputeCapabilityMajor, device));
+  RAFT_CUDA_TRY(cudaDeviceGetAttribute(&minor, cudaDevAttrComputeCapabilityMinor, device));
+
+  std::string archs = "-arch=sm_" + std::to_string((major * 10 + minor));
+
+  // Load the generated LTO IR and link them together
+  nvJitLinkHandle handle;
+  const char* lopts[] = {"-lto", archs.c_str()};
+  auto result         = nvJitLinkCreate(&handle, 2, lopts);
+  check_nvjitlink_result(handle, result);
+
+  for (auto& frag : this->fragments) {
+    frag->add_to(handle);
+  }
+
+  // Call to nvJitLinkComplete causes linker to link together all the LTO-IR
+  // modules perform any optimizations and generate cubin from it.
+  result = nvJitLinkComplete(handle);
+  check_nvjitlink_result(handle, result);
+
+  // get cubin from nvJitLink
+  size_t cubin_size;
+  result = nvJitLinkGetLinkedCubinSize(handle, &cubin_size);
+  check_nvjitlink_result(handle, result);
+
+  std::unique_ptr<char[]> cubin{new char[cubin_size]};
+  result = nvJitLinkGetLinkedCubin(handle, cubin.get());
+  check_nvjitlink_result(handle, result);
+
+  result = nvJitLinkDestroy(&handle);
+  RAFT_EXPECTS(result == NVJITLINK_SUCCESS, "nvJitLinkDestroy failed");
+
+  // cubin is linked, so now load it
+  // NOTE: cudaLibrary_t does not need to be freed explicitly
+  cudaLibrary_t library;
+  RAFT_CUDA_TRY(
+    cudaLibraryLoadData(&library, cubin.get(), nullptr, nullptr, 0, nullptr, nullptr, 0));
+
+  constexpr unsigned int count = 1;
+  // NOTE: cudaKernel_t does not need to be freed explicitly
+  std::unique_ptr<cudaKernel_t[]> kernels{new cudaKernel_t[count]};
+  RAFT_CUDA_TRY(cudaLibraryEnumerateKernels(kernels.get(), count, library));
+
+  return std::make_shared<AlgorithmLauncher>(kernels.release()[0]);
+}
