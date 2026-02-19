4b76753baa840f3c0ca3e39b519cbadfaf7b0332
aamijar <aamijar230@gmail.com>
2026-02-19 13:28:14 -0800

    remove unused transform_iterator.h

--- cpp/src/neighbors/detail/knn_brute_force.cuh
+++ cpp/src/neighbors/detail/knn_brute_force.cuh
@@ -42,7 +42,6 @@
 #include <rmm/device_uvector.hpp>
 #include <thrust/for_each.h>
 #include <thrust/iterator/counting_iterator.h>
-#include <thrust/iterator/transform_iterator.h>
 
 #include <cstdint>
 #include <iostream>
