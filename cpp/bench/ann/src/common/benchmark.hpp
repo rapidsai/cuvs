618a3dce91b7c842f05e2d0b211dcee1b84abd21
James Xia <jamxia@nvidia.com>
2026-02-20 04:18:28 -0800

    Use std::thread instead of OMP

--- cpp/bench/ann/src/common/benchmark.hpp
+++ cpp/bench/ann/src/common/benchmark.hpp
@@ -13,8 +13,6 @@
 #include <benchmark/benchmark.h>
 #include <unistd.h>
 
-#include <omp.h>
-
 #include <algorithm>
 #include <chrono>
 #include <cmath>
@@ -374,45 +372,73 @@ void bench_search(::benchmark::State& state,
     // We go through the groundtruth with same stride as the benchmark loop.
     size_t out_offset   = 0;
     size_t batch_offset = (state.thread_index() * n_queries) % query_set_size;
-    // Avoid CPU oversubscription when calculating match_count with OMP
-    uint32_t num_probe_threads = std::thread::hardware_concurrency() / benchmark_n_threads;
+    // Avoid CPU oversubscription when parallelizing recall calculation loop
+    int num_recall_calculation_worker_threads =
+      std::thread::hardware_concurrency() / benchmark_n_threads - 1;  // -1 for the main thread
+    num_recall_calculation_worker_threads =
+      std::max(num_recall_calculation_worker_threads,
+               0);  // ensure non-negative number of workers (possible if hardware_concurrency()
+                    // does not return an expected value)
     while (out_offset < rows && out_offset <= max_queries_for_recall) {
-#pragma omp parallel for reduction(+ : match_count, total_count) num_threads(num_probe_threads)
-      for (std::size_t i = 0; i < n_queries; i++) {
-        size_t i_orig_idx = batch_offset + i;
-        size_t i_out_idx  = out_offset + i;
-        if (i_out_idx < rows) {
-          /* NOTE: recall correctness & filtering
+      std::vector<std::thread> recall_calculation_workers;
+      recall_calculation_workers.reserve(num_recall_calculation_worker_threads);
+      std::vector<std::size_t> local_match_count(num_recall_calculation_worker_threads + 1);
+      std::vector<std::size_t> local_total_count(num_recall_calculation_worker_threads + 1);
+      int chunk_size =
+        n_queries / (num_recall_calculation_worker_threads + 1);  // +1 for the main thread
+      int remainder           = n_queries % (num_recall_calculation_worker_threads + 1);
+      auto recall_calculation = [&](int start, int end, int tid) {
+        for (int i = start; i < end; ++i) {
+          size_t i_orig_idx = batch_offset + i;
+          size_t i_out_idx  = out_offset + i;
+          if (i_out_idx < rows) {
+            /* NOTE: recall correctness & filtering
 
-          We generate the filtered ground truth values and build an unordered_set with them to
-          enable O(1) lookup. We need enough ground truth values to compute recall correctly though.
-          But the ground truth file only contains `max_k` values per row; if there are less valid
-          values than k among them, we overestimate the recall. Essentially, we compare the first
-          `filter_pass_count` values of the algorithm output, and this counter can be less than
-          `k`. In the extreme case of very high filtering rate, we may be bypassing entire rows of
-          results. However, this is still better than no recall estimate at all.
+            We generate the filtered ground truth values and build an unordered_set with them to
+            enable O(1) lookup. We need enough ground truth values to compute recall correctly
+            though. But the ground truth file only contains `max_k` values per row; if there are
+            less valid values than k among them, we overestimate the recall. Essentially, we compare
+            the first `filter_pass_count` values of the algorithm output, and this counter can be
+            less than `k`. In the extreme case of very high filtering rate, we may be bypassing
+            entire rows of results. However, this is still better than no recall estimate at all.
 
-          */
-          std::unordered_set<std::int32_t> gt_set;
-          if (!filter_bitset) { gt_set.reserve(max_k); }
-          uint32_t filter_pass_count{};
-          for (std::uint32_t l = 0; l < max_k && filter_pass_count < k; l++) {
-            auto exp_idx = gt[i_orig_idx * max_k + l];
-            if (!filter(exp_idx)) { continue; }
-            gt_set.insert(exp_idx);
-            filter_pass_count++;
+            */
+            std::unordered_set<std::int32_t> gt_set;
+            if (!filter_bitset) { gt_set.reserve(max_k); }
+            uint32_t filter_pass_count{};
+            for (std::uint32_t l = 0; l < max_k && filter_pass_count < k; l++) {
+              auto exp_idx = gt[i_orig_idx * max_k + l];
+              if (!filter(exp_idx)) { continue; }
+              gt_set.insert(exp_idx);
+              filter_pass_count++;
+            }
+
+            local_total_count[tid] += filter_pass_count;
+
+            for (std::uint32_t j = 0; j < k; j++) {
+              auto act_idx = static_cast<std::int32_t>(neighbors_host[i_out_idx * k + j]);
+              if (gt_set.count(act_idx)) local_match_count[tid]++;
+            }
           }
-
-          total_count += filter_pass_count;
-
-          size_t match_count_local = 0;
-          for (std::uint32_t j = 0; j < k; j++) {
-            auto act_idx = static_cast<std::int32_t>(neighbors_host[i_out_idx * k + j]);
-            if (gt_set.count(act_idx)) match_count_local++;
-          }
-          match_count += match_count_local;
         }
+      };
+      // launch worker threads
+      int start = 0;
+      for (int tid = 0; tid < num_recall_calculation_worker_threads; tid++) {
+        int end = start + chunk_size;
+        if (tid < remainder) { ++end; }
+        recall_calculation_workers.emplace_back(recall_calculation, start, end, tid);
+        start = end;
       }
+      // main thread works on last chunk
+      recall_calculation(start, n_queries, num_recall_calculation_worker_threads);
+      // join all worker threads
+      for (auto& worker : recall_calculation_workers) {
+        worker.join();
+      }
+      match_count += std::accumulate(local_match_count.begin(), local_match_count.end(), 0);
+      total_count += std::accumulate(local_total_count.begin(), local_total_count.end(), 0);
+
       out_offset += n_queries;
       batch_offset = (batch_offset + queries_stride) % query_set_size;
     }
