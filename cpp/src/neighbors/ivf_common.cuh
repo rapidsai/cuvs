135c05b3a439a78011c4e313dd094ca751b466cc
Yan Zaretskiy <yzaretskiy@nvidia.com>
2026-03-05 19:53:40 +0000

    Fix the postprocess_distances L1 branch to match the recent main branch refactor

--- cpp/src/neighbors/ivf_common.cuh
+++ cpp/src/neighbors/ivf_common.cuh
@@ -237,14 +237,15 @@ void postprocess_distances(const raft::resources& res,
     case distance::DistanceType::BitwiseHamming: break;
     case distance::DistanceType::L1: {
       if (scaling_factor != 1.0) {
-        raft::linalg::unaryOp(out,
-                              in,
-                              len,
-                              raft::compose_op(raft::mul_const_op<ScoreOutT>{scaling_factor},
-                                               raft::cast_op<ScoreOutT>{}),
-                              stream);
+        raft::linalg::map(
+          res,
+          out_view,
+          raft::compose_op(raft::mul_const_op<ScoreOutT>{scaling_factor},
+                           raft::cast_op<ScoreOutT>{}),
+          raft::make_const_mdspan(in_view));
       } else if (needs_cast || needs_copy) {
-        raft::linalg::unaryOp(out, in, len, raft::cast_op<ScoreOutT>{}, stream);
+        raft::linalg::map(
+          res, out_view, raft::cast_op<ScoreOutT>{}, raft::make_const_mdspan(in_view));
       }
     } break;
     default: RAFT_FAIL("Unexpected metric.");
