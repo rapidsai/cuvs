38a880f615788fac28817f9d65bebfa3ed9644c0
aamijar <aamijar230@gmail.com>
2026-03-13 23:44:44 +0000

    fix deselect paths

--- ci/run_cuvs_pytests.sh
+++ ci/run_cuvs_pytests.sh
@@ -7,4 +7,4 @@ set -euo pipefail
 # Support invoking run_pytests.sh outside the script directory
 cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")"/../python/cuvs/cuvs
 
-PYTHONUNBUFFERED=1 pytest --cache-clear -vs --deselect tests/test_hnsw_ace.py::test_hnsw_ace_tiny_memory_limit_triggers_disk_mode --deselect tests/test_cagra_ace.py::test_cagra_ace_tiny_memory_limit_triggers_disk_mode "$@" tests
+PYTHONUNBUFFERED=1 pytest --cache-clear -vs --deselect cuvs/tests/test_hnsw_ace.py::test_hnsw_ace_tiny_memory_limit_triggers_disk_mode --deselect cuvs/tests/test_cagra_ace.py::test_cagra_ace_tiny_memory_limit_triggers_disk_mode "$@" tests
