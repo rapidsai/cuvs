0bb8b21e8b93912107dc5b8a4ccbd261708adb54
aamijar <aamijar230@gmail.com>
2026-03-18 07:45:12 +0000

    update to_raft_params

--- cpp/src/preprocessing/pca/detail/pca.cuh
+++ cpp/src/preprocessing/pca/detail/pca.cuh
@@ -16,13 +16,9 @@ namespace cuvs::preprocessing::pca::detail {
 /**
  * @brief Convert cuvs::preprocessing::pca::params to raft::linalg::paramsPCA.
  */
-inline auto to_raft_params(const params& config, std::size_t n_rows, std::size_t n_cols)
-  -> raft::linalg::paramsPCA
+inline auto to_raft_params(const params& config) -> raft::linalg::paramsPCA
 {
   raft::linalg::paramsPCA prms;
-  prms.n_rows       = n_rows;
-  prms.n_cols       = n_cols;
-  prms.n_components = config.n_components;
   prms.algorithm    = config.algorithm;
   prms.tol          = config.tol;
   prms.n_iterations = config.n_iterations;
@@ -43,7 +39,7 @@ void fit(raft::resources const& handle,
          raft::device_scalar_view<DataT, IndexT> noise_vars,
          bool flip_signs_based_on_U)
 {
-  auto raft_prms = to_raft_params(config, input.extent(0), input.extent(1));
+  auto raft_prms = to_raft_params(config);
   raft::linalg::pca_fit(handle,
                         raft_prms,
                         input,
@@ -69,7 +65,7 @@ void fit_transform(raft::resources const& handle,
                    raft::device_scalar_view<DataT, IndexT> noise_vars,
                    bool flip_signs_based_on_U)
 {
-  auto raft_prms = to_raft_params(config, input.extent(0), input.extent(1));
+  auto raft_prms = to_raft_params(config);
   raft::linalg::pca_fit_transform(handle,
                                   raft_prms,
                                   input,
@@ -92,7 +88,7 @@ void transform(raft::resources const& handle,
                raft::device_vector_view<DataT, IndexT> mu,
                raft::device_matrix_view<DataT, IndexT, raft::col_major> trans_input)
 {
-  auto raft_prms = to_raft_params(config, input.extent(0), input.extent(1));
+  auto raft_prms = to_raft_params(config);
   raft::linalg::pca_transform(handle, raft_prms, input, components, singular_vals, mu, trans_input);
 }
 
@@ -105,7 +101,7 @@ void inverse_transform(raft::resources const& handle,
                        raft::device_vector_view<DataT, IndexT> mu,
                        raft::device_matrix_view<DataT, IndexT, raft::col_major> output)
 {
-  auto raft_prms = to_raft_params(config, output.extent(0), output.extent(1));
+  auto raft_prms = to_raft_params(config);
   raft::linalg::pca_inverse_transform(
     handle, raft_prms, trans_input, components, singular_vals, mu, output);
 }
