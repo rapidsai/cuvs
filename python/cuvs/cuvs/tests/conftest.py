b1303416955383b042d4587907eb8b7ae0dede7b
Howard Huang <howardhuangb@gmail.com>
2026-04-15 14:05:36 -0700

    fix copyright

--- python/cuvs/cuvs/tests/conftest.py
+++ python/cuvs/cuvs/tests/conftest.py
@@ -1,3 +1,6 @@
+# SPDX-FileCopyrightText: Copyright (c) 2025-2026, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
 # arm tests sporadically run into
 # https://bugzilla.redhat.com/show_bug.cgi?id=1722181.
 # This is a workaround to ensure that OpenMP gets the TLS that it needs.
