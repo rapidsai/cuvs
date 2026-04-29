0bf92c0192f56a265048f44febfd1fd53c39cf46
jakirkham <jakirkham@gmail.com>
2026-04-29 14:28:15 -0700

    Drop extra copy in `get_last_error_text`
    Cython can run `decode` directly on `const char*`. So do that instead of
    copying to `bytes` first before calling `decode`. This saves a copy.

--- python/cuvs/cuvs/common/exceptions.pyx
+++ python/cuvs/cuvs/common/exceptions.pyx
@@ -16,8 +16,7 @@ def get_last_error_text():
     cdef const char* c_err = cuvsGetLastErrorText()
     if c_err is NULL:
         return
-    cdef bytes err = c_err
-    return err.decode("utf8", "ignore")
+    return c_err.decode("utf8", "ignore")
 
 
 def check_cuvs(status: cuvsError_t):
