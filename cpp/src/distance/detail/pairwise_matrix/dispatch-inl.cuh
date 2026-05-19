f9ff9e83600a258f2a2e8c25e5ec7c8b7f8b6763
tarang-jain <tarangj@nvidia.com>
2026-05-18 17:51:06 -0700

    update arch_include

--- cpp/src/distance/detail/pairwise_matrix/dispatch-inl.cuh
+++ cpp/src/distance/detail/pairwise_matrix/dispatch-inl.cuh
@@ -4,15 +4,61 @@
  */
 #pragma once
 
-/* This file provides concise function templates that can be instantiated in
- * src/distance/detail/pairwise_matrix/. Previously,
- * cuvs::distance::detail::distance was instantiated. The function necessarily
- * required a large set of include files, which slowed down the build.
+/* This file has two responsibilities:
+ *
+ * 1. Dispatch to the correct implementation of a kernel based on the
+ *    architecture of the device on which the kernel will be launched. For
+ *    instance, the cosine distance has a CUTLASS-based implementation that can
+ *    be used on SM80+ and the JIT implementation that is used on older
+ *    architectures.
+ *
+ * 2. Provide concise function templates that can be instantiated in
+ *    src/distance/detail/pairwise_matrix/. Previously,
+ *    cuvs::distance::detail::distance was instantiated. The function
+ *    necessarily required a large set of include files, which slowed down the
+ *    build.
  */
 
+#include "../distance_ops/cutlass.cuh"                               // ops::has_cutlass_op
 #include "../pairwise_matrix/jit_lto_kernels/pairwise_matrix_jit.cuh"  // pairwise_matrix_jit_dispatch
+#include <raft/core/error.hpp>                                      // RAFT_CUDA_TRY
+#include <raft/util/arch.cuh>                                       // raft::util::arch::SM_*
+
+// NOTE: to minimize compile times, we do not include dispatch_sm80.cuh.
+// Including dispatch_sm80.cuh can slow down compile times (due to CUTLASS).
+// Therefore, it is the including file's responsibility to include
+// dispatch_sm80.cuh for CUTLASS-backed distance ops, as is done in
+// src/distance/detail/pairwise_matrix/dispatch_*.cu.
 
 namespace cuvs::distance::detail {
+
+// This forward-declaration ensures that we do not need to include
+// dispatch_sm80.cuh if we are not calling it in practice. This makes compiling
+// all the non-CUTLASS based distance instantiations faster. For CUTLASS-based
+// distances, dispatch_sm80.cuh has to be included by the file including this
+// file.
+template <typename OpT,
+          typename IdxT,
+          typename DataT,
+          typename OutT,
+          typename FinOpT,
+          typename SM_compat_t>
+void pairwise_matrix_sm80_dispatch(OpT,
+                                   pairwise_matrix_params<IdxT, DataT, OutT, FinOpT>,
+                                   SM_compat_t,
+                                   cudaStream_t);
+
+inline auto current_device_arch()
+{
+  int device = 0;
+  RAFT_CUDA_TRY(cudaGetDevice(&device));
+
+  int major = 0;
+  int minor = 0;
+  RAFT_CUDA_TRY(cudaDeviceGetAttribute(&major, cudaDevAttrComputeCapabilityMajor, device));
+  RAFT_CUDA_TRY(cudaDeviceGetAttribute(&minor, cudaDevAttrComputeCapabilityMinor, device));
+  return raft::util::arch::SM(major * 10 + minor);
+}
 
 template <typename OpT,
           typename DataT,
@@ -43,7 +89,30 @@ void pairwise_matrix_dispatch(OpT distance_op,
 
   if (!params.is_row_major) { params.flip_x_and_y(); }
 
-  pairwise_matrix_jit_dispatch(distance_op, params, stream);
+  // Dispatch rule:
+  // - execute CUTLASS-based kernel on SM_80 and above when the op supports it
+  // - execute JIT kernel otherwise
+  namespace arch = raft::util::arch;
+
+  constexpr bool cutlass_op_unavailable = !ops::has_cutlass_op<OpT>::value;
+
+  if constexpr (cutlass_op_unavailable) {
+    pairwise_matrix_jit_dispatch(distance_op, params, stream);
+  } else {
+    auto cutlass_range = arch::SM_range(arch::SM_80(), arch::SM_future());
+    auto runtime_arch  = current_device_arch();
+
+    // TODO: CUTLASS does not support odd `k` with half DataT.
+    bool unsupported_half = (sizeof(DataT) == 2) && ((k % 2) != 0);
+
+    if (!unsupported_half && cutlass_range.contains(runtime_arch)) {
+      // If device is SM_80 or later, use CUTLASS-based kernel.
+      pairwise_matrix_sm80_dispatch(distance_op, params, cutlass_range, stream);
+      return;
+    }
+
+    pairwise_matrix_jit_dispatch(distance_op, params, stream);
+  }
 }
 
 };  // namespace cuvs::distance::detail
