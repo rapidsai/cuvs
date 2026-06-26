909e6c048329060231ec3b4b547eb7339844121e
Yan Zaretskiy <yzaretskiy@nvidia.com>
2026-06-26 14:29:08 -0700

    Fix copyright headers

--- rust/cuvs/src/ivf_pq/mod.rs
+++ rust/cuvs/src/ivf_pq/mod.rs
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: Copyright (c) 2024-2026, NVIDIA CORPORATION.
+ * SPDX-FileCopyrightText: Copyright (c) 2024-2026, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
  * SPDX-License-Identifier: Apache-2.0
  */
 //! IVF-PQ: an inverted-file index that product-quantizes the vectors. Like
